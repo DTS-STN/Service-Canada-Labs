7f8164d7710c36fa038cb6562961d644f65a7eb7
EphraimAssi <1868295@cegepoutaouais.qc.ca>
2021-03-18 12:46:21 -0400

    Home page Markup

--- pages/index.js
+++ pages/index.js
@@ -3,63 +3,117 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Alpha Site</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <header>
+        <figure>
+          <img src="#" alt="logo.jpg"></img>
+          <figcaption>
+            Governement of Canada
+            Gouvernement du Canada
+          </figcaption>
+        </figure>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <section>
+          <a href="#">Français</a>
+          <input type="search" placeholder="Search Canada.ca"></input>
+        </section>
+      </header>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+      <main>
+        <nav>Menu</nav>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+        <section>
+          <figure>
+            <img src="#" alt="serviceCanada.jpg"></img>
+            <h1>
+              Servica Canada Labs
+              Make government work for you
+            </h1>
+            <figcaption>
+              <button>Become a tester</button>
+              <button>Start testing</button>
+            </figcaption>
+          </figure>
+        </section>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+        <section>
+          <h2>About us</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus consectetur urna nec tempor tristique.
+            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+            Nulla at condimentum odio, quis porttitor eros. Quisque laoreet pretium purus, sed maximus turpis.
+            Quisque ut magna vel dui placerat aliquet in non orci.
+          </p>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+          <figure>
+            Block
+          </figure>
+
+          <figure>
+            Block
+            <figcaption>
+              Diagram that shows what we do and how user feedback
+              gets transformed in to a service to Canadians
+              - and/or -
+              Benefits of joining the user pool for testers
+            </figcaption>
+          </figure>
+        </section>
+
+        <section>
+          <h3>Contact us</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus consectetur urna nec tempor tristique.</p>
+          <a href="#">john.doe@example.com</a>
+          <figure>
+            Block
+          </figure>
+        </section>
+
+        <section>
+          <h4>Featured prototypes</h4>
+          <figure>
+            Block
+          </figure>
+          <figure>
+            Block
+          </figure>
+          <figure>
+            Block
+          </figure>
+        </section>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+      <footer>
+        <figure>
+          Experiencing an issue with this product or have you spotted an error?
+            <figcaption>
+            <button>Report a problem</button>
+          </figcaption>
+        </figure>
+
+        <ul>
+          <li>
+            <a href="#">Social media</a>
+          </li>
+          <li>
+            <a href="#">Mobile applications</a>
+          </li>
+          <li>
+            <a href="#">Terms and conditions</a>
+          </li>
+          <li>
+            <a href="#">Privacy</a>
+          </li>
+        </ul>
+
+        <img src="#" alt="canada.jpg"></img>
       </footer>
+
     </div>
   )
 }
