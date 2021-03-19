6294bb541f870218ee24aed8f583683e4984513f
Benoit Cloutier <benoit.bc.cloutier@gmail.com>
2021-03-19 16:42:54 -0400

    Adding accessibility text for background image.

--- components/atoms/Banner.js
+++ components/atoms/Banner.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Banner = ({ siteTitle, headLine }) => {
   return (
     // <div className="provisional bg-banner bg-cover wb-init wb-bgimg-inited" id="wb-auto-4">
-    <div className="bg-banner-img py-8 md:px-8">
+    <div title="Banner image." className="bg-banner-img py-8 md:px-8">
       <div className="container w-full md:w-3/4 xl:w-1/2 bg-dk-blue bg-opacity-90 text-white p-4">
         <h1 className="text-4xl pt-4 pb-2">{siteTitle}</h1>
         <hr className="border-2 border-red-600 bg-red-600 border-opacity-90 w-3/4" />
