a5b0b108aa7bd4060683b0cc911c0b2fb39e3f0f
Moro-Code <omar@omarnasr.ca>
2021-03-19 12:26:25 -0400

    cypress, jest, github action

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/", "<rootDir>/coverage/"],
+    testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/", "<rootDir>/cypress/"],
     setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
     transform: {
         "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
