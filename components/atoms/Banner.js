a4737e0f8979c1e78de73c977c605e21d132cc9c
Benoit Cloutier <benoit.bc.cloutier@gmail.com>
2021-03-19 17:07:43 -0400

    Adding story and tests. Still not fully functional

--- components/atoms/Banner.js
+++ components/atoms/Banner.js
@@ -1,8 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
+
+/**
+ * Displays the banner on the page
+ */
 
 export const Banner = ({ siteTitle, headLine }) => {
   return (
-    // <div className="provisional bg-banner bg-cover wb-init wb-bgimg-inited" id="wb-auto-4">
     <div title="Banner image." className="bg-banner-img py-8 md:px-8">
       <div className="container w-full md:w-3/4 xl:w-1/2 bg-dk-blue bg-opacity-90 text-white p-4">
         <h1 className="text-4xl pt-4 pb-2">{siteTitle}</h1>
@@ -11,6 +15,18 @@ export const Banner = ({ siteTitle, headLine }) => {
       </div>
     </div>
   );
+};
+
+Banner.propTypes = {
+  /**
+   * Text area that displays the title for the page
+   */
+  siteTitle: PropTypes.string.isRequired,
+
+  /**
+   * This is for adding an id for testing in cypress
+   */
+  headLine: PropTypes.string.isRequired,
 };
 
 export default Banner;
---
+++ components/atoms/Banner.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+import { Primary } from "./Banner.stories";
+
+it("renders Banner in its primary state", () => {
+  render(<Primary {...Primary.args} />);
+  const textElement = screen.getByText("Banner Text");
+  const headLineElement = screen.getByText("Banner Headline");
+  expect(textElement).toBeTruthy();
+  expect(headLineElement).toBeTruthy();
+});
