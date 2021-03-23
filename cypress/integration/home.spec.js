4b48444b852d1a99f9b2aa08eaa38d30948b25ec
will0684 <jordan.walcaraz@gmail.com>
2021-03-23 11:18:05 -0400

    Added cypress-axe and jest-axe with report config

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,12 +1,19 @@
+import terminalLog from '../plugins/terminalLog'
+
 /* eslint-disable no-undef */
 /// <reference types="Cypress" />
 
 describe("home page", () => {
     beforeEach(() => {
         cy.visit("/");
+        cy.injectAxe();
     });
 
     it("landing page loaded", () =>{
         cy.url().should("contains", "/");
+    })
+
+    it('Has no detectable a11y violations on load', () => {
+        cy.checkA11y(null, null, terminalLog)
     })
 })
---
+++ cypress/plugins/terminalLog.js
@@ -0,0 +1,20 @@
+// Define at the top of the spec file or just import it
+export default function terminalLog(violations) {
+  cy.task(
+    'log',
+    `${violations.length} accessibility violation${
+      violations.length === 1 ? '' : 's'
+    } ${violations.length === 1 ? 'was' : 'were'} detected`
+  )
+  // pluck specific keys to keep the table readable
+  const violationData = violations.map(
+    ({ id, impact, description, nodes }) => ({
+      id,
+      impact,
+      description,
+      nodes: nodes.length
+    })
+  )
+
+  cy.task('table', violationData)
+}
