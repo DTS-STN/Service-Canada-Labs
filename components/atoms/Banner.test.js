c189d11f20c907ac08db68a4745365aca3cf91d0
Benoit Cloutier <benoit.bc.cloutier@gmail.com>
2021-03-24 13:52:48 -0400

    Added a11y tests.

--- components/atoms/Banner.test.js
+++ components/atoms/Banner.test.js
@@ -1,13 +1,23 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-
+import { axe, toHaveNoViolations } from "jest-axe";
 import { Primary } from "./Banner.stories";
 
-it("renders Banner in its primary state", () => {
-  render(<Primary {...Primary.args} />);
-  const textElement = screen.getByText("Banner Text");
-  const headlineElement = screen.getByText("Banner Headline");
-  expect(textElement).toBeTruthy();
-  expect(headlineElement).toBeTruthy();
+expect.extend(toHaveNoViolations);
+
+describe("Banner tests", () => {
+  it("renders Banner in its primary state", () => {
+    render(<Primary {...Primary.args} />);
+    const textElement = screen.getByText("Banner Text");
+    const headlineElement = screen.getByText("Banner Headline");
+    expect(textElement).toBeTruthy();
+    expect(headlineElement).toBeTruthy();
+  });
+
+  it("has no a11y violations", async () => {
+    const { container } = render(<Primary {...Primary.args} />);
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
+  });
 });
