c5934e521715c467f64e062b9b5c1963984dce8b
Benoit Cloutier <benoit.bc.cloutier@gmail.com>
2021-03-24 10:43:10 -0400

    At 768px and less, dark-blue bg is full width.

--- components/atoms/Banner.js
+++ components/atoms/Banner.js
@@ -5,13 +5,13 @@ import PropTypes from "prop-types";
  * Displays the banner on the page
  */
 
-export const Banner = ({ siteTitle, headLine }) => {
+export const Banner = ({ siteTitle, headline }) => {
   return (
     <div title="Banner image." className="bg-banner-img py-8 md:px-8">
-      <div className="container w-full md:w-3/4 xl:w-1/2 bg-dk-blue text-white p-4">
+      <div className="md:container w-full md:w-3/4 xl:w-1/2 bg-dk-blue text-white p-4">
         <h1 className="text-4xl pt-4 pb-2 break-words">{siteTitle}</h1>
         <hr className="border-2 border-hr-red-bar bg-hr-red-bar w-3/4" />
-        <p className="text-2xl py-4 break-words">{headLine}</p>
+        <p className="text-2xl py-4 break-words">{headline}</p>
       </div>
     </div>
   );
