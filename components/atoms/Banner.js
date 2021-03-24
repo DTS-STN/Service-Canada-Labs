3b7ae2e0c247d4e2e27aacb7b08b50f99d75dc16
Benoit Cloutier <benoit.bc.cloutier@gmail.com>
2021-03-24 09:40:39 -0400

    Added worp-break to banner.

--- components/atoms/Banner.js
+++ components/atoms/Banner.js
@@ -9,9 +9,9 @@ export const Banner = ({ siteTitle, headLine }) => {
   return (
     <div title="Banner image." className="bg-banner-img py-8 md:px-8">
       <div className="container w-full md:w-3/4 xl:w-1/2 bg-dk-blue bg-opacity-90 text-white p-4">
-        <h1 className="text-4xl pt-4 pb-2">{siteTitle}</h1>
+        <h1 className="text-4xl pt-4 pb-2 break-words">{siteTitle}</h1>
         <hr className="border-2 border-red-600 bg-red-600 border-opacity-90 w-3/4" />
-        <p className="text-2xl py-4">{headLine}</p>
+        <p className="text-2xl py-4 break-words">{headLine}</p>
       </div>
     </div>
   );
