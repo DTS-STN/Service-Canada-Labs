6a071c5985529c6dea55ef87d8d381dd4a15de55
Jordan Willis <31868510+will0684@users.noreply.github.com>
2021-03-24 10:14:33 -0400

    cypress-axe and jest-axe (#11)
    * Added cypress-axe and jest-axe with report config
    
    * Added custom document to support html lang attribute
    
    * reverted package-lock
    
    * reverted package-lock
    
    * Added cypress action to workflow
    
    * Added build step to test workflow
    
    * edit comment

---
+++ pages/_document.js
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+// Used to augment application's html and body tags
+// Read more here: https://nextjs.org/docs/advanced-features/custom-document
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
