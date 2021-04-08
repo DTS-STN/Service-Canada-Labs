b04e18b72a9cea0bee02102959ea3a26f745e6fb
EphraimAssi <72703030+EphraimAssi@users.noreply.github.com>
2021-04-08 12:59:25 -0400

    Bread crumb (#24)
    * Added a breadcrumb functional breadcrumb component
    
    * Few adjustements
    
    * Merge main
    
    * Few changes
    
    * Doing Testing

---
+++ components/atoms/Breadcrumb.test.js
@@ -0,0 +1,28 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { axe, toHaveNoViolations } from "jest-axe";
+import { Primary, WithItems } from "./Breadcrumb.stories";
+
+expect.extend(toHaveNoViolations);
+
+describe("BreadCrumb", () => {
+  it("renders primary", () => {
+    const primary = render(<Primary {...Primary.args} />);
+    expect(primary).toBeTruthy();
+  });
+
+  it("renders breadcrumb with items", () => {
+    render(<WithItems {...WithItems.args} />);
+
+    WithItems.args.items.forEach((value) => {
+      screen.getByText(value.text);
+    });
+  });
+
+  it("has no a11y violations", async () => {
+    const { container } = render(<Primary {...Primary.args} />);
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
+  });
+});
