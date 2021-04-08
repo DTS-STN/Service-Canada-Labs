ed9a8b8b90284a12cd1b047da56f9467f7fa12ec
will0684 <jordan.walcaraz@gmail.com>
2021-04-08 14:24:17 -0400

    Added translation content files, added language content provider in _app.js

---
+++ i18n/fr.js
@@ -0,0 +1,20 @@
+export const table = {
+  siteTitle: "Site Alpha",
+  bannerTitle: "Laboratoires de Service Canada",
+  bannerText: "Faites travailler le gouvernement pour vous",
+  bannerBecomeTester: "Devenez testeur",
+  bannerStartTesting: "Commencer les tests",
+  aboutUsHeading: "à propos de nous",
+  aboutUsContent: "(FR) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus consectetur urna nec tempor tristique. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla at condimentum odio, quis porttitor eros. Quisque laoreet pretium purus, sed maximus turpis. Quisque ut magna vel dui placerat aliquet in non orci.",
+  figCaption: "Diagramme qui montre ce que nous faisons et comment les commentaires des utilisateurs sont transformés en un service pour les Canadiens - et / ou - les avantages de rejoindre le bassin d'utilisateurs pour les testeurs",
+  contactUsHeading: "Nous contacter",
+  contactUsContent: "(FR) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus consectetur urna nec tempor tristique.",
+  featuredPrototypesHeading: "Prototypes en vedette",
+  footerReportProblemButtonString: "Signaler un problème",
+  footerReportProblem: "Vous rencontrez un problème avec ce produit ou avez-vous repéré une erreur?",
+  footerSocialMedia: "Des médias sociaux",
+  footerMobileApplications: "Applications mobiles",
+  footerTermsAndConditions: "Termes et conditions",
+  footerPrivacy: "Intimité",
+  searchBarPlaceholder: "Rechercher Canada.ca"
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
+import { I18nProvider } from "next-rosetta";
 import "../icomoon/style.css";
 import "../styles/globals.css";
 import "../styles/fonts.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <I18nProvider table={pageProps.table}>
+      <Component {...pageProps} />
+    </I18nProvider>
+  );
 }
 
 export default MyApp;
