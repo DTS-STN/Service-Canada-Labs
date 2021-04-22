49039b643d45481dfe253164f30d46d6a8959ba5
will0684 <jordan.walcaraz@gmail.com>
2021-04-22 11:13:11 -0400

    match PhaseBanner font-sizes to match mockup

--- components/atoms/PhaseBanner.js
+++ components/atoms/PhaseBanner.js
@@ -9,11 +9,13 @@ export const PhaseBanner = ({ phase, children }) => {
   return (
     <div className="bg-gray-100 sm:max-h-14">
       <div className="xxs:w-screen xxs:mx-0 xxs:px-0 lg:container  lg:mx-auto lg:px-0 xxl:mx-auto">
-        <div className="flex items-start text-xxs p-4 sm:text-xs">
-          <span className="font-body uppercase tracking-normal border-2 border-black px-2 lg:ml-2">
+        <div className="flex items-start p-4">
+          <span className="font-body text-sm uppercase tracking-normal border-2 border-black px-2 lg:ml-2">
             {phase}
           </span>
-          <span className="font-body break-words ml-4 pt-1">{children}</span>
+          <span className="font-body text-xs break-words ml-4 pt-1">
+            {children}
+          </span>
         </div>
       </div>
     </div>
