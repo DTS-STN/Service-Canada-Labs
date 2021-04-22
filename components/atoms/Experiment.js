02829440ca7415a855561338e9d998d459a9259d
will0684 <jordan.walcaraz@gmail.com>
2021-04-22 14:14:51 -0400

    use Noto sans for span text

--- components/atoms/Experiment.js
+++ components/atoms/Experiment.js
@@ -9,7 +9,7 @@ export const Experiment = ({ title, tag, desc }) => {
   return (
     <div className="shadow-experimentShadow px-4 py-6 border-b-4 border-custom-blue-experimentBlue">
       <h3 className="mb-2 text-p">{title}</h3>
-      <span className="inline-block py-2 px-2 uppercase text-xxs text-white font-bold bg-custom-blue-experimentBlue rounded">
+      <span className="inline-block py-2 px-2 uppercase font-body text-xxs text-white font-bold bg-custom-blue-experimentBlue rounded">
         {tag}
       </span>
       <p className="mt-2 text-sm">{desc}</p>
