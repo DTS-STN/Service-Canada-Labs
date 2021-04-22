4f3af05f05c3380c8813d2bcf73a49926bb1c9be
Moro-Code <omar@omarnasr.ca>
2021-04-22 12:03:27 -0400

    modify button string

--- components/molecules/Details.js
+++ components/molecules/Details.js
@@ -6,7 +6,7 @@ import PropTypes from "prop-types";
 export function Details(props) {
   return (
     <details data-testid={props.dataTestId} data-cy={props.dataCy}>
-      <summary className="h-46px max-w-350px w-full bg-details-button-gray focus:bg-details-button-active-gray hover:bg-details-button-active-gray rounded py-12px px-5px font-body text-sm text-center text-canada-footer-font cursor-pointer border border-outset border-details-button-gray">
+      <summary className="h-46px max-w-350px w-full bg-details-button-gray focus:bg-details-button-active-gray hover:bg-details-button-active-gray rounded py-12px px-5px font-body text-xs text-center text-canada-footer-font cursor-pointer border border-outset border-details-button-gray">
         {props.label}
       </summary>
       <div className="max-w-350px w-full min-h-200px bg-custom-blue-details mt-1 p-15px">
