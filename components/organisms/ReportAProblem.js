110f75209e5d05af06a69cd90c1de3849a9f0e5e
Moro-Code <omar@omarnasr.ca>
2021-04-22 12:08:04 -0400

    check all that apply margin size

--- components/organisms/ReportAProblem.js
+++ components/organisms/ReportAProblem.js
@@ -41,7 +41,7 @@ export function ReportAProblem(props) {
         <form className="w-full" action="#" onSubmit={onSubmitHandler}>
           <input type="hidden" id="language" value={i18n.language} />
           <fieldset>
-            <legend className="text-base sm:text-p font-body font-normal mb-4">
+            <legend className="text-base sm:text-p font-body font-normal mb-6">
               {t("reportAProblemCheckAllThatApply")}
             </legend>
             <OptionalTextField
