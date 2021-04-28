599760090d5f78d7ab002c3eef0bb3d18178b377
Benoit Cloutier <benoit.bc.cloutier@gmail.com>
2021-04-28 13:29:29 -0400

    Working on Experiment page layout.

--- pages/experiments.js
+++ pages/experiments.js
@@ -1,13 +1,24 @@
 import Head from "next/head";
+import {useState} from 'react';
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
 import { Layout } from "../components/organisms/Layout";
+import { Experiment } from "../components/atoms/Experiment";
 import { useRouter } from "next/router";
 
 export default function experiments(props) {
   const { t } = useTranslation("common");
   const { asPath } = useRouter();
+  const [experimentList, setExperimentList] = useState([
+    {id:1, title:"Title Placeholder", tag:"Active", desc:"This experiment is a placeholder for when the real content comes in. experiment is a placeholder for when the real content comes in"},
+    {id:2, title:"Title Placeholder", tag:"Active", desc:"This experiment is a placeholder for when the real content gggggg dffef iment is a placeholder for when the real content comes in"},
+    {id:3, title:"Title Placeholder", tag:"Comming Soon", desc:"This experiment is a placeholder for when the real content comes in. experiment is a placeholder for when the real content comes in"},
+    {id:4, title:"Title Placeholder", tag:"Active", desc:"tent comes in. experiment is a placeholder for when the real content comes in"},
+    {id:5, title:"Title Placeholder", tag:"Comming Soon", desc:"This experiment is a placeholder for when the re placeholder for when the real content comes in"},
+  ]);
+  
 
+    const displayExperiments = experimentList.map((experiment) => <Experiment key={experiment.id} title={experiment.title} tag={experiment.tag} desc={experiment.desc} />);
   return (
     <Layout locale={props.locale} langUrl={asPath}>
       <Head>
@@ -17,7 +28,15 @@ export default function experiments(props) {
       <section className="layout-container">
         <h1>{t("experimentsAndExplorationTitle")}</h1>
         <p>Filter Experiments component goes here</p>
-        <p>List of experiments.</p>
+        
+        <div className="">
+        {displayExperiments}
+        {/* <Experiment title="Title placeholder" tag="Active" desc="This experiment is a placeholder for when the real content comes in. experiment is a placeholder for when the real content comes in" />
+        <Experiment title="Title placeholder" tag="Active" desc="This experiment is a placeholder for when the real content comes in. xperiment is a placeholder for when the real content comes in" />
+        <Experiment title="Title placeholder" tag="Comming Soon" desc="This experiment is a placeholder for when the real content comes in xperiment is a placeholder for when the real content comes in." />
+        <Experiment title="Title placeholder" tag="Active" desc="This experiment is a placeholder for when the real content comes in." />
+        <Experiment title="Title placeholder" tag="Comming Soon" desc="This experiment is a placeholder for when the real content comes in. xperiment is a placeholder for when the real content comes in" /> */}
+        </div>
       </section>
     </Layout>
   );
