736a79e3efff253f95e4700d4c52b859320c4316
will0684 <jordan.walcaraz@gmail.com>
2021-05-10 11:50:54 -0400

    changed breakpoint for image and section content

--- pages/about.js
+++ pages/about.js
@@ -15,7 +15,7 @@ export default function About(props) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <section className="layout-container relative mb-28">
-        <div className="hidden lg:block">
+        <div className="hidden xl:block">
           <img
             className="absolute right-0 h-96"
             src="/lightbulb_gradient.png"
@@ -23,7 +23,7 @@ export default function About(props) {
         </div>
         <h1 className="mb-8 text-h1l font-bold">{t("aboutTitle")}</h1>
         <h2 className="mb-6 text-h1 font-bold">{t("aboutThisSiteHeader")}</h2>
-        <div className="lg:w-2/3">
+        <div className="xl:w-2/3">
           <p className="mb-4">{t("aboutThisSiteContent1")}</p>
           <p>{t("aboutThisSiteContent2")}</p>
         </div>
