e7c5df8bc6952badf2feee594bb54d89f3e9419a
Moro-Code <omar@omarnasr.ca>
2021-05-17 14:25:15 -0400

    remove image opts config

--- .storybook/main.js
+++ .storybook/main.js
@@ -14,17 +14,6 @@ module.exports = {
     "@storybook/addon-postcss",
   ],
   webpackFinal: (config) => {
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        "process.env.__NEXT_IMAGE_OPTS": JSON.stringify({
-          deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-          imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-          domains: [],
-          path: "/",
-          loader: "default",
-        }),
-      })
-    );
     // useTranslation() hook in next-i18next is looking for a server environment and storybooks
     // runs client-side, so use client-side react-i18next to allow components to render outside server environment
     config.resolve.alias = {
