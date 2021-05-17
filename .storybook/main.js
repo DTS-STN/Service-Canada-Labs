4fdea641b860fbf014a3dd35ac62607031a7bbc5
Omar Nasr <omar@omarnasr.ca>
2021-05-17 11:10:08 -0400

    Add .storybook path to the lint path. Add replicated next image compo… (#63)
    * Add .storybook path to the lint path. Add replicated next image component
    
    * update to husky v6 and set up commit hook

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,35 +2,35 @@ var webpack = require("webpack");
 var path = require("path");
 
 module.exports = {
-  "stories": [
+  stories: [
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-    "../components/**/*/*.stories.@(js|jsx|ts|tsx)"
+    "../components/**/*/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-a11y",
-    "@storybook/addon-postcss"
+    "@storybook/addon-postcss",
   ],
   webpackFinal: (config) => {
     config.plugins.push(
-        new webpack.DefinePlugin({
-          "process.env.__NEXT_IMAGE_OPTS": JSON.stringify({
-            deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-            imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-            domains: [],
-            path: "/",
-            loader: "default",
-          }),
-        })
+      new webpack.DefinePlugin({
+        "process.env.__NEXT_IMAGE_OPTS": JSON.stringify({
+          deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+          imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+          domains: [],
+          path: "/",
+          loader: "default",
+        }),
+      })
     );
     // useTranslation() hook in next-i18next is looking for a server environment and storybooks
     // runs client-side, so use client-side react-i18next to allow components to render outside server environment
     config.resolve.alias = {
       ...config.resolve.alias,
-      'next-i18next': 'react-i18next'
-    }
+      "next-i18next": "react-i18next",
+    };
     return config;
-  }
-}
+  },
+};
