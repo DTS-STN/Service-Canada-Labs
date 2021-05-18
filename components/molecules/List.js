16ae3b7484fea6bcc791201fc0cb977897dc7433
EphraimAssi <1868295@cegepoutaouais.qc.ca>
2021-05-18 16:51:40 -0400

    List component

---
+++ components/molecules/List.js
@@ -0,0 +1,43 @@
+import PropTypes from "prop-types";
+
+/**
+ *  List component
+ */
+export function List(props) {
+  let opacity = 20;
+  return (
+    <ol className={props.className}>
+      {props.items.map((item, key) => {
+        opacity += 20;
+        return (
+          <li key={key} className="flex">
+            <span
+              className={
+                "opacity-" +
+                opacity +
+                " bg-circle-color flex-shrink-0 mr-4 mb-2 rounded-full h-32 w-32 flex items-center justify-center text-white font-bold text-h1xl relative md:left-0 -left-14"
+              }
+            >
+              {key + 1}
+            </span>
+            <p className="text-sm md:text-p my-auto leading-normal font-body">
+              {item}
+            </p>
+          </li>
+        );
+      })}
+    </ol>
+  );
+}
+
+List.propTypes = {
+  /**
+   * List items
+   */
+  items: PropTypes.arrayOf(String).isRequired,
+
+  /**
+   * Option for styling component
+   */
+  className: PropTypes.string,
+};
