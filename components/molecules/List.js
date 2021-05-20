48e4a9393d37e9f1e46d663e6dcee3da03a0e4c9
EphraimAssi <1868295@cegepoutaouais.qc.ca>
2021-05-20 14:12:02 -0400

    Lint fix

--- components/molecules/List.js
+++ components/molecules/List.js
@@ -9,13 +9,14 @@ export function List(props) {
     <ol className={props.className}>
       {props.items.map((item, key) => {
         if (opacity < 100) opacity += 20;
-        let className="opacity-" + opacity + " bg-circle-color flex-shrink-0 mr-4 mb-2 rounded-full h-32 w-32 flex items-center justify-center text-white font-bold text-h1xl relative md:left-0 -left-14"
+        let className =
+          "opacity-" +
+          opacity +
+          " bg-circle-color flex-shrink-0 mr-4 mb-2 rounded-full h-32 w-32 flex items-center justify-center text-white font-bold text-h1xl relative md:left-0 -left-14";
 
         return (
           <li key={key} className="flex">
-            <span className={className}>
-              {key + 1}
-            </span>
+            <span className={className}>{key + 1}</span>
             <p className="text-sm md:text-p my-auto leading-normal font-body">
               {item}
             </p>
