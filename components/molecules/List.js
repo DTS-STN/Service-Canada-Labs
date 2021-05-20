5558fad67a290f138b54335d51b4057092d65c4d
EphraimAssi <1868295@cegepoutaouais.qc.ca>
2021-05-20 13:37:20 -0400

    Circle color

--- components/molecules/List.js
+++ components/molecules/List.js
@@ -10,17 +10,13 @@ export function List(props) {
       {props.items.map((item, key) => {
         if (circleColor < 4) circleColor += 1;
 
+        let className =
+          "bg-circle-color-" +
+          circleColor +
+          " flex-shrink-0 mr-4 mb-2 rounded-full h-32 w-32 flex items-center justify-center text-white font-bold text-h1xl relative md:left-0 -left-14";
         return (
           <li key={key} className="flex">
-            <span
-              className={
-                "bg-circle-color" +
-                circleColor +
-                " flex-shrink-0 mr-4 mb-2 rounded-full h-32 w-32 flex items-center justify-center text-white font-bold text-h1xl relative md:left-0 -left-14"
-              }
-            >
-              {key + 1}
-            </span>
+            <span className={className}>{key + 1}</span>
             <p className="text-sm md:text-p my-auto leading-normal font-body">
               {item}
             </p>
