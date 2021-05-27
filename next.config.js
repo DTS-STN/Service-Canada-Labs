5a766c3612a4ae3ce93e5d6da25a0a2188a9f4a3
will0684 <jordan.walcaraz@gmail.com>
2021-05-27 13:02:31 -0400

    env var config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,5 @@ module.exports = {
   env: {
     NEXT_PUBLIC_BUILD_DATE: process.env.NEXT_PUBLIC_BUILD_DATE,
     NEXT_PUBLIC_TC_BUILD: process.env.NEXT_PUBLIC_TC_BUILD,
-    STRAPI_API_BACKEND_URL: process.env.STRAPI_API_BACKEND_URL,
   }
 }
