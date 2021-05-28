be50e6ecbc610212eae9ad3bbaf94aaa4d790235
EphraimAssi <1868295@cegepoutaouais.qc.ca>
2021-05-28 14:52:15 -0400

    Report a problem bug

--- pages/404.js
+++ pages/404.js
@@ -25,24 +25,24 @@ export default function About(props) {
             <div className="relative h-auto xl:w-96 xxl:w-400px lg:w-72 xl:h-400px lg:h-500px mb-8 lg:mb-0">
               <p className="text-h1 font-bold font-display mb-4">
                 We couldn't find that Web page
-            </p>
+              </p>
               <p className="font-bold font-body mb-8">Error 404</p>
               <p className="font-body text-sm mb-4 leading-30px">
                 We're sorry you ended up here. Sometimes a page gets moved or
                 deleted, but hopefully we can help you find what you're looking
                 for. What next?
-            </p>
+              </p>
               <div className="flex">
                 <span className="error404-link" />
                 <p className="font-body text-sm leading-30px">
                   Return to the &nbsp;
-              <Link href="/">
+                  <Link href="/">
                     <a className="underline hover:text-canada-footer-hover-font-blue text-canada-footer-font">
                       Service Canada Labs home page
-                </a>
+                    </a>
                   </Link>
-              ;
-              </p>
+                  ;
+                </p>
               </div>
             </div>
             <ReportAProblem />
@@ -58,24 +58,24 @@ export default function About(props) {
             <div className="relative h-auto xl:w-96 xxl:w-400px lg:w-72 xl:h-400px lg:h-500px mb-8 lg:mb-0">
               <p className="text-h1 font-bold font-display mb-4">
                 Nous ne pouvons trouver cette page Web
-            </p>
+              </p>
               <p className="font-bold font-body mb-8">Error 404</p>
               <p className="font-body text-sm mb-4 leading-30px">
                 Nous sommes désolés que vous ayez abouti ici. Il arrive parfois
                 qu'une page ait été déplacée ou supprimée. Heureusement, nous
                 pouvons vous aider à trouver ce que vous cherchez. Que faire?
-            </p>
+              </p>
               <div className="flex">
                 <span className="error404-link" />
                 <p className="font-body text-sm leading-30px">
                   Retournez à la &nbsp;
-              <Link href="/">
+                  <Link href="/">
                     <a className="underline hover:text-canada-footer-hover-font-blue text-canada-footer-font">
                       page d'accueil [Service Canada Labs]
-                </a>
-              </Link>
-              ;
-            </p>
+                    </a>
+                  </Link>
+                  ;
+                </p>
               </div>
             </div>
             <ReportAProblem />
