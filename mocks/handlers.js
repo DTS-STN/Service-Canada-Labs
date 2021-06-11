40a39f382350deb917a49187cfa974c51b7d0e13
will0684 <jordan.walcaraz@gmail.com>
2021-06-11 12:13:12 -0400

    changed e2e testing mock handler to env variable

--- mocks/handlers.js
+++ mocks/handlers.js
@@ -2,7 +2,7 @@ import { rest } from 'msw'
 import experimentsFixture from '../cypress/fixtures/experiments.json'
 
 export const handlers = [
-  rest.get('https://alphasite-api.dts-stn.com/experiments', async (req, res, ctx) => {
+  rest.get(`${process.env.STRAPI_API_BACKEND_URL}/experiments`, async (req, res, ctx) => {
     return res(
       ctx.json(experimentsFixture)
     )
