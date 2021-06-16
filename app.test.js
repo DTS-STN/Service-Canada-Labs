76b73f4fe25d8a416a452975ac4733dd9c45dac1
var-kyle <kyle.meelker@gmail.com>
2021-06-16 12:37:42 -0400

    remove redirect to splash page. fix unit test. fix some a11y issues on splash page

--- app.test.js
+++ app.test.js
@@ -1,15 +1,14 @@
 /**
  * @jest-environment jsdom
  */
-import {render, screen} from "@testing-library/react"
-import App from "./pages/index"
-
+import { render, screen } from "@testing-library/react";
+import App from "./pages/index";
 
 describe("App", () => {
-    it("renders without crashing", () => {
-        render(<App />);
-        expect(
-            screen.getByRole("heading", { name: "bannerTitle" })
-        ).toBeInTheDocument();
-    });
+  it("renders without crashing", () => {
+    render(<App />);
+    expect(
+      screen.getByRole("heading", { name: "alpha.service.canada.ca" })
+    ).toBeInTheDocument();
+  });
 });
