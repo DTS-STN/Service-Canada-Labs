019000debf355ddb37f56a6b9ad25cb4a4dcafab
var-kyle <kyle.meelker@gmail.com>
2021-06-28 09:29:57 -0400

    add unit tests for custom error page

--- pages/error.js
+++ pages/error.js
@@ -20,82 +20,102 @@ export default function ErrorPage(props) {
         <meta name="dcterms.creator" content={t("creator")} />
         <meta name="dcterms.accessRights" content="2" />
       </Head>
-      <section className="layout-container pb-44">
-        <img
-          className="h-auto w-60 pt-6 xl:w-96 xxl:w-1/2"
-          src={"/sig-blk-en.svg"}
-          alt={"Symbol of the Government of Canada"}
-        />
-        <div className="flex flex-col lg:flex-row justify-between items-center lg:items-start mt-8">
-          <div>
-            <div className="relative h-auto xl:w-96 xxl:w-400px lg:w-72 xl:h-400px lg:h-500px mb-8 lg:mb-0">
-              <h1 className="font-bold font-display mb-4">
-                {props.errorTitle}
-              </h1>
-              {props.statusCode ? (
-                <p className="font-bold font-body mb-8">
-                  Error {props.statusCode}
+      <main>
+        <section className="layout-container pb-44">
+          <img
+            className="h-auto w-60 pt-6 xl:w-96 xxl:w-1/2"
+            src={"/sig-blk-en.svg"}
+            alt={"Symbol of the Government of Canada"}
+          />
+          <div className="flex flex-col lg:flex-row justify-between items-center lg:items-start mt-8">
+            <div>
+              <div className="relative h-auto xl:w-96 xxl:w-400px lg:w-72 xl:h-400px lg:h-500px mb-8 lg:mb-0">
+                <h1
+                  className="font-bold font-display mb-4"
+                  data-testid="heading-en"
+                >
+                  {props.errorTitle}
+                </h1>
+                {props.statusCode ? (
+                  <p
+                    className="font-bold font-body mb-8"
+                    data-testid="statuscode-en"
+                  >
+                    Error {props.statusCode}
+                  </p>
+                ) : (
+                  ""
+                )}
+                <p
+                  className="font-body text-sm mb-4 leading-30px"
+                  data-testid="errormessage-en"
+                >
+                  {props.errorMessage}
                 </p>
-              ) : (
-                ""
-              )}
-              <p className="font-body text-sm mb-4 leading-30px">
-                {props.errorMessage}
-              </p>
-              <div className="flex">
-                <span className="error404-link" />
-                <p className="font-body text-sm leading-30px">
-                  Return to the &nbsp;
-                  <Link href="/">
-                    <a className="underline hover:text-canada-footer-hover-font-blue text-canada-footer-font">
-                      Service Canada Labs home page
-                    </a>
-                  </Link>
-                  ;
-                </p>
+                <div className="flex">
+                  <span className="error404-link" />
+                  <p className="font-body text-sm leading-30px">
+                    Return to the &nbsp;
+                    <Link href="/">
+                      <a className="underline hover:text-canada-footer-hover-font-blue text-canada-footer-font">
+                        Service Canada Labs home page
+                      </a>
+                    </Link>
+                    ;
+                  </p>
+                </div>
               </div>
+              <ReportAProblem language={"en"} />
             </div>
-            <ReportAProblem language={"en"} />
-          </div>
-          <div className="flex items-center justify-center circle-background my-8 lg:mt-0">
-            <img
-              className="w-68px lg:w-12 xl:100px"
-              src="/crackedbulb.svg"
-              alt="Cracked lightbulb"
-            />
-          </div>
-          <div>
-            <div className="relative h-auto xl:w-96 xxl:w-400px lg:w-72 xl:h-400px lg:h-500px mb-8 lg:mb-0">
-              <h1 className="font-bold font-display mb-4">
-                {props.errorTitleFr}
-              </h1>
-              {props.statusCode ? (
-                <p className="font-bold font-body mb-8">
-                  Erreur {props.statusCode}
+            <div className="flex items-center justify-center circle-background my-8 lg:mt-0">
+              <img
+                className="w-68px lg:w-12 xl:100px"
+                src="/crackedbulb.svg"
+                alt="Cracked lightbulb"
+              />
+            </div>
+            <div>
+              <div className="relative h-auto xl:w-96 xxl:w-400px lg:w-72 xl:h-400px lg:h-500px mb-8 lg:mb-0">
+                <h1
+                  className="font-bold font-display mb-4"
+                  data-testid="heading-fr"
+                >
+                  {props.errorTitleFr}
+                </h1>
+                {props.statusCode ? (
+                  <p
+                    className="font-bold font-body mb-8"
+                    data-testid="statuscode-fr"
+                  >
+                    Erreur {props.statusCode}
+                  </p>
+                ) : (
+                  ""
+                )}
+                <p
+                  className="font-body text-sm mb-4 leading-30px"
+                  data-testid="errormessage-fr"
+                >
+                  {props.errorMessageFr}
                 </p>
-              ) : (
-                ""
-              )}
-              <p className="font-body text-sm mb-4 leading-30px">
-                {props.errorMessageFr}
-              </p>
-              <div className="flex">
-                <span className="error404-link" />
-                <p className="font-body text-sm leading-30px">
-                  Retournez à la &nbsp;
-                  <Link href="/fr">
-                    <a className="underline hover:text-canada-footer-hover-font-blue text-canada-footer-font">
-                      page d'accueil [Service Canada Labs]
-                    </a>
-                  </Link>
-                  ;
-                </p>
+                <div className="flex">
+                  <span className="error404-link" />
+                  <p className="font-body text-sm leading-30px">
+                    Retournez à la &nbsp;
+                    <Link href="/fr">
+                      <a className="underline hover:text-canada-footer-hover-font-blue text-canada-footer-font">
+                        page d'accueil [Service Canada Labs]
+                      </a>
+                    </Link>
+                    ;
+                  </p>
+                </div>
               </div>
+              <ReportAProblem language="fr" />
             </div>
-            <ReportAProblem language="fr" />
           </div>
-        </div>
-      </section>
+        </section>
+      </main>
       <footer className="h-100px w-screen bg-footer-background-color absolute bottom-0">
         <div className="layout-container flex justify-between lg:flex-row-reverse pt-4 lg:pt-0 lg:mt-8">
           <ActionButton
