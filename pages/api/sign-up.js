d844f19806fca869de81e93527b68671a41eec37
EphraimAssi <1868295@cegepoutaouais.qc.ca>
2021-07-07 16:30:41 -0400

    French link for validation email

--- pages/api/sign-up.js
+++ pages/api/sign-up.js
@@ -46,7 +46,9 @@ async function handler(req, res) {
 
     // attempt to send validation email through notify
     try {
-      const validationUrl = origin + `/api/validate?id=${userCuid}`;
+      var lang = "";
+      data.language === "fr" ? (lang = "/fr") : (lang = "");
+      const validationUrl = origin + `${lang}/api/validate?id=${userCuid}`;
       const [status, json] = await submitEmail(
         {
           validation_url: validationUrl,
