81f757f2e9a9dfff74912546b146090ee4b76fb7
EphraimAssi <1868295@cegepoutaouais.qc.ca>
2021-07-08 15:39:43 -0400

    merge main issues

--- pages/api/sign-up.js
+++ pages/api/sign-up.js
@@ -46,10 +46,7 @@ async function handler(req, res) {
 
     // attempt to send validation email through notify
     try {
-      let lang = "";
-      data.language === "en" ? (lang = "") : (lang = "fr");
-      const validationUrl =
-        origin + `/api/validate?id=${userCuid}&lang=${lang}`;
+      const validationUrl = origin + `/api/validate?id=${userCuid}`;
       const [status, json] = await submitEmail(
         {
           validation_url: validationUrl,
