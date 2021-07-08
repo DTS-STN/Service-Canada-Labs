c59a0e570409c8aafa31c76eb9f802909a046f56
Benoit Cloutier <benoit.bc.cloutier@gmail.com>
2021-07-08 15:18:42 -0400

    Expired and Wrong URL error handled.

--- pages/error.js
+++ pages/error.js
@@ -28,8 +28,9 @@ export default function ErrorPage(props) {
             alt={"Symbol of the Government of Canada"}
           />
           <div className="flex flex-col lg:flex-row justify-between items-center lg:items-start mt-8">
+            {/* Left Side (English section) */}
             <div>
-              <div className="relative h-auto xl:w-96 xxl:w-400px lg:w-72 xl:h-400px lg:h-500px mb-8 lg:mb-0">
+              <div className="relative h-auto xl:w-96 xxl:w-400px lg:w-72 lg:h-500px mb-8 lg:mb-0">
                 <h1
                   className="font-bold font-display mb-4"
                   data-testid="heading-en"
@@ -47,23 +48,81 @@ export default function ErrorPage(props) {
                   ""
                 )}
                 <p
-                  className="font-body text-sm mb-4 leading-30px"
+                  className="font-body text-p font-bold mb-4 leading-30px"
                   data-testid="errormessage-en"
                 >
                   {props.errorMessage}
                 </p>
-                <div className="flex">
-                  <span className="error404-link" />
-                  <p className="font-body text-sm leading-30px">
-                    Return to the &nbsp;
-                    <Link href="/">
-                      <a className="underline hover:text-canada-footer-hover-font-blue text-canada-footer-font">
-                        Service Canada Labs home page
-                      </a>
-                    </Link>
-                    ;
-                  </p>
-                </div>
+                {props.errorMessage === "Wrong URL" ? (
+                  <>
+                    {/* Wrong URL English Section */}
+                    <p className="font-body text-sm leading-30px mb-5">
+                      It may be due to an invalid ID or missing ID. What’s next?
+                    </p>
+                    <ul>
+                      <li className="flex">
+                        <span className="error404-link" />
+                        <p className="font-body text-sm leading-30px">
+                          Try clicking the link again from your email;
+                        </p>
+                      </li>
+                      <li className="flex">
+                        <span className="error404-link" />
+                        <p className="font-body text-sm leading-30px">
+                          Contact us at{" "}
+                          <a
+                            href={`mailto:${process.env.NEXT_PUBLIC_NOTIFY_REPORT_A_PROBLEM_EMAIL}`}
+                            className="text-custom-blue-link underline"
+                          >
+                            experience@service.gc.ca
+                          </a>{" "}
+                          and we’ll help you out.
+                        </p>
+                      </li>
+                    </ul>
+                    <p className="font-body text-sm leading-30px mt-5">
+                      Thank you for your patience.
+                    </p>
+                  </>
+                ) : props.errorMessage === "Expired URL" ? (
+                  <>
+                    {/* Expired URL English Section */}
+                    <p className="font-body text-sm leading-30px mb-5">
+                      It may be due to an expired URL if it has been more than
+                      24 hours since you received the email to verify your email
+                      address. What’s next?
+                    </p>
+                    <ul>
+                      <li className="flex">
+                        <span className="error404-link" />
+                        <p className="font-body text-sm leading-30px">
+                          <Link href="/signup">
+                            <a className="underline hover:text-canada-footer-hover-font-blue text-canada-footer-font">
+                              Sign up
+                            </a>
+                          </Link>{" "}
+                          again
+                        </p>
+                      </li>
+                    </ul>
+                    <p className="font-body text-sm leading-30px mt-5">
+                      Thank you for your patience.
+                    </p>
+                  </>
+                ) : (
+                  /* General Error English Section */
+                  <div className="flex">
+                    <span className="error404-link" />
+                    <p className="font-body text-sm leading-30px">
+                      Return to the &nbsp;
+                      <Link href="/">
+                        <a className="underline hover:text-canada-footer-hover-font-blue text-canada-footer-font">
+                          Service Canada Labs home page
+                        </a>
+                      </Link>
+                    </p>
+                  </div>
+                )}
               </div>
               <ReportAProblem language={"en"} />
             </div>
@@ -74,9 +133,10 @@ export default function ErrorPage(props) {
                 alt="Cracked lightbulb"
               />
             </div>
+            {/* Right Side (French section) */}
             <div>
               <div
-                className="relative h-auto xl:w-96 xxl:w-400px lg:w-72 xl:h-400px lg:h-500px mb-8 lg:mb-0"
+                className="relative h-auto xl:w-96 xxl:w-400px lg:w-72 lg:h-500px mb-8 lg:mb-0"
                 lang="fr"
               >
                 <h1
@@ -96,23 +156,83 @@ export default function ErrorPage(props) {
                   ""
                 )}
                 <p
-                  className="font-body text-sm mb-4 leading-30px"
+                  className="font-body text-p font-bold mb-4 leading-30px"
                   data-testid="errormessage-fr"
                 >
                   {props.errorMessageFr}
                 </p>
-                <div className="flex">
-                  <span className="error404-link" />
-                  <p className="font-body text-sm leading-30px">
-                    Retournez à la &nbsp;
-                    <Link href="/fr">
-                      <a className="underline hover:text-canada-footer-hover-font-blue text-canada-footer-font">
-                        page d'accueil [Service Canada Labs]
-                      </a>
-                    </Link>
-                    ;
-                  </p>
-                </div>
+                {props.errorMessageFr === "URL erronée" ? (
+                  <>
+                    {/* Wrong URL French Section */}
+                    <p className="font-body text-sm leading-30px mb-5">
+                      Cela peut être dû à un identificateur invalide ou à un
+                      identificateur manquant. Que faire?
+                    </p>
+                    <ul>
+                      <li className="flex">
+                        <span className="error404-link" />
+                        <p className="font-body text-sm leading-30px">
+                          Essayez de cliquer à nouveau sur le lien depuis votre
+                          courriel;
+                        </p>
+                      </li>
+                      <li className="flex">
+                        <span className="error404-link" />
+                        <p className="font-body text-sm leading-30px">
+                          Contactez nous à{" "}
+                          <a
+                            href={`mailto:${process.env.NEXT_PUBLIC_NOTIFY_REPORT_A_PROBLEM_EMAIL}`}
+                            className="text-custom-blue-link underline"
+                          >
+                            experience@service.gc.ca
+                          </a>{" "}
+                          et nous vous aiderons.
+                        </p>
+                      </li>
+                    </ul>
+                    <p className="font-body text-sm leading-30px mt-5">
+                      Merci de votre patience.
+                    </p>
+                  </>
+                ) : props.errorMessageFr === "URL expirée" ? (
+                  <>
+                    {/* Expired URL French Section */}
+                    <p className="font-body text-sm leading-30px mb-5">
+                      Le problème peut être dû à une URL expirée si cela fait
+                      plus de 24 heures que vous avez reçu le courriel vérifier
+                      votre adresse de courrier électronique. Que faire?
+                    </p>
+                    <ul>
+                      <li className="flex">
+                        <span className="error404-link" />
+                        <p className="font-body text-sm leading-30px">
+                          <Link href="/signup">
+                            <a className="underline hover:text-canada-footer-hover-font-blue text-canada-footer-font">
+                              Inscrivez-vous
+                            </a>
+                          </Link>{" "}
+                          de nouveau
+                        </p>
+                      </li>
+                    </ul>
+                    <p className="font-body text-sm leading-30px mt-5">
+                      Merci de votre patience.
+                    </p>
+                  </>
+                ) : (
+                  /* General Error French Section */
+                  <div className="flex">
+                    <span className="error404-link" />
+                    <p className="font-body text-sm leading-30px">
+                      Retournez à la &nbsp;
+                      <Link href="/fr">
+                        <a className="underline hover:text-canada-footer-hover-font-blue text-canada-footer-font">
+                          page d'accueil [Service Canada Labs]
+                        </a>
+                      </Link>
+                    </p>
+                  </div>
+                )}
               </div>
               <ReportAProblem language="fr" />
             </div>
