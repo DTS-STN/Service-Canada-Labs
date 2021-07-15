1e619d844aa24026f4ca6ede00f07d35b5a20a1b
Benoit Cloutier <benoit.bc.cloutier@gmail.com>
2021-07-15 11:28:19 -0400

    Adding Phasebanner back.

---
+++ components/atoms/PhaseBanner.js
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+/**
+ * Displays the PhaseBanner on the page
+ */
+
+export const PhaseBanner = ({ phase, children }) => {
+  return (
+    <div className="bg-circle-color">
+      <div className="block sm:flex py-4 layout-container">
+        <span className="font-body text-xs text-circle-color font-extrabold flex-shrink-0 bg-white px-4 py-1 my-auto leading-6">
+          {phase}
+        </span>
+        <p className="font-body text-xs mt-5 sm:mt-auto text-white sm:ml-4 pt-1 my-auto">
+          {children}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+PhaseBanner.propTypes = {
+  /**
+   * Phase stage in the PhaseBanner
+   */
+  phase: PropTypes.string.isRequired,
+  /**
+   * Phase stage in the PhaseBanner
+   */
+  children: PropTypes.string.isRequired,
+};
+
+export default PhaseBanner;
