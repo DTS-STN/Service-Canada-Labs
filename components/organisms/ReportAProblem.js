5f21985cb90ac8d4cb1119189bfbc929febfefa5
EphraimAssi <72703030+EphraimAssi@users.noreply.github.com>
2021-07-16 15:16:41 -0400

    Analytics implementation (#204)
    * Changing script placement
    
    * Report a problem tracking
    
    * Merging main
    
    * Page issued attribute
    
    * Submit button
    
    * Submit and reset button tracking
    
    * Report a problem form name
    
    * Submit reset fix
    
    * Submit reset fix

--- components/organisms/ReportAProblem.js
+++ components/organisms/ReportAProblem.js
@@ -60,7 +60,7 @@ export function ReportAProblem(props) {
         </>
       ) : (
         <form
-          data-gc-analytics-formname="ESDC:ServiceCanadaLabsReport-Problem"
+          data-gc-analytics-formname="ESDC|EDSC:ServiceCanadaLabsReport-Problem"
           data-gc-analytics-collect='[{"value":"input,select","emptyField":"N/A"}]'
           className="w-full"
           action="#"
@@ -279,6 +279,7 @@ export function ReportAProblem(props) {
             type="submit"
             dataCy="report-a-problem-submit"
             dataTestId="report-a-problem-submit"
+            analyticsTracking
           >
             {t("reportAProblemSubmit", { lng: props.language })}
           </ActionButton>
