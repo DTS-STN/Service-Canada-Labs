4c8ed9cb8335e6ab2d8c69310a0afc8694d5c551
Jordan Willis <31868510+will0684@users.noreply.github.com>
2021-07-19 15:42:33 -0400

    Headers, Subresource Integrity and Content Security Policy Config (#211)
    * added CSP to meta tg in master document
    
    * Added security related headers to the applicion and SRI support in webpack
    
    * addedCSP header for X-Frame-Options
    
    * get jquery from CDN with HTTPS
    
    * changed NODE_ENV in CI pipeline to "ci"
    
    * changed NODE_ENV in CI pipeline to "development"
    
    * added check for dev or CI environment
    
    * add CI env var to build stage
    
    * fixed env check
    
    * remove NODE_ENV value for Cypress stage
    
    * log dev or CI check
    
    * log out environment variables
    
    * logging more vars
    
    * modify env check
    
    * fix variable name
    
    * log check method
    
    * modify env check
    
    * change string format
    
    * log environment
    
    * use string for CI check
    
    * remove console.logs

--- pages/_document.js
+++ pages/_document.js
@@ -2,6 +2,26 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 
 /* istanbul ignore file */
 
+function getCsp() {
+  let csp = ``;
+  csp += `base-uri 'self';`;
+  csp += `form-action 'self';`;
+  csp += `default-src 'self' dts-stn.com *.dts-stn.com;`;
+  csp += `script-src 'self' ${
+    process.env.CI === "true"
+      ? "'unsafe-eval'"
+      : process.env.NODE_ENV === "development"
+      ? "'unsafe-eval'"
+      : ""
+  } https://ajax.googleapis.com;`; // NextJS requires 'unsafe-eval' in dev (faster source maps)
+  csp += `style-src 'self' 'unsafe-inline' https://fonts.googleapis.com data:;`; // NextJS requires 'unsafe-inline'
+  csp += `img-src 'self';`;
+  csp += `font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com;`;
+  // csp += `frame-src *;` // TODO
+  // csp += `media-src *;` // TODO
+  return csp;
+}
+
 // Used to augment application's html and body tags
 // Read more here: https://nextjs.org/docs/advanced-features/custom-document
 class MyDocument extends Document {
@@ -13,11 +33,13 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <meta httpEquiv="Content-Security-Policy" content={getCsp()} />
+        </Head>
         <body>
           <Main />
           <NextScript />
-          <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
           {process.env.NEXT_PUBLIC_ADOBE_ANALYTICS_URL ? (
             <script type="text/javascript">_satellite.pageBottom()</script>
           ) : (
