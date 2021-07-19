cf898024184451c03228731f13dcfe00d645e84b
Kris Charbonneau <char0799@algonquinlive.com>
2021-07-19 10:19:49 -0400

    Fixed typo in test

--- __tests__/pages/life-journeys.test.js
+++ __tests__/pages/life-journeys.test.js
@@ -2,11 +2,11 @@
  * @jest-environment jsdom
  */
 import { render, screen } from "@testing-library/react";
-import Privacy from "../../pages/privacy";
+import LifeJourneys from "../../pages/projects/life-journeys";
 
 describe("Life Journeys Prototype", () => {
   it("renders without crashing", () => {
-    render(<Privacy />);
+    render(<LifeJourneys />);
     expect(screen.getByText("lifeJourneysTitle")).toBeInTheDocument();
   });
 });
