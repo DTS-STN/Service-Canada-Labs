232e134b1eb13cd9d608a04cfbb497ee5078d2d6
var-kyle <kyle.meelker@gmail.com>
2021-07-21 09:12:22 -0400

    fix home page unit test

--- __tests__/pages/home.test.js
+++ __tests__/pages/home.test.js
@@ -7,8 +7,6 @@ import Home from "../../pages/home";
 describe("Home", () => {
   it("renders without crashing", () => {
     render(<Home />);
-    expect(
-      screen.getByText("experimentsAndExplorationTitle")
-    ).toBeInTheDocument();
+    expect(screen.getByText("projectsAndExplorationTitle")).toBeInTheDocument();
   });
 });
