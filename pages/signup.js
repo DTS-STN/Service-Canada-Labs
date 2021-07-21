b008e0178c099a9058484981f4ece57061723243
EphraimAssi <1868295@cegepoutaouais.qc.ca>
2021-07-21 14:38:55 -0400

    Lint fix

--- pages/signup.js
+++ pages/signup.js
@@ -383,7 +383,12 @@ export default function Signup(props) {
     <Layout
       locale={props.locale}
       langUrl={asPath}
-      breadcrumbItems={[{ text: t("bannerTitle"), link: props.locale === "en" ? "/home" : "/fr/home"}]}
+      breadcrumbItems={[
+        {
+          text: t("bannerTitle"),
+          link: props.locale === "en" ? "/home" : "/fr/home",
+        },
+      ]}
     >
       <Head>
         {process.env.NEXT_PUBLIC_ADOBE_ANALYTICS_URL ? (
