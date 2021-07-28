53e2fb592398810b6e3e32c08186725c2f33117c
will0684 <jordan.walcaraz@gmail.com>
2021-07-28 11:05:05 -0400

    increase font size of call to action description to 16px on mobile

--- components/molecules/CallToAction.js
+++ components/molecules/CallToAction.js
@@ -11,7 +11,7 @@ export function CallToAction(props) {
   return (
     <aside>
       <div className="bg-circle-color text-white">
-        <div className="layout-container pb-10 pt-10 text-xs md:text-base">
+        <div className="layout-container pb-10 pt-10 text-sm md:text-base">
           <h2>{props.title}</h2>
           <div className="flex flex-col lg:grid lg:grid-cols-2 lg:gap-24 gap-5">
             {props.description ? (
