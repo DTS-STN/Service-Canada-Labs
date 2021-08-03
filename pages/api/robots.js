49865f2621c217f2d720205325bb5f7b52cb3c24
var-kyle <kyle.meelker@gmail.com>
2021-08-03 09:19:47 -0400

    add robots api route and rewrite config

---
+++ pages/api/robots.js
@@ -0,0 +1,16 @@
+/**
+ * Disallow web crawlers to scrape our dev site, while only blocking crawlers from scraping the api routes in production
+ * @param req
+ * @param res
+ * @returns {Promise<void>}
+ */
+export default async function handler(req, res) {
+  if (process.env.ENVIRONMENT === "production") {
+    res.write("User-agent: *\n");
+    res.write("Disallow: /api/*\n");
+  } else {
+    res.write("User-agent: *\n");
+    res.write("Disallow: /\n");
+  }
+  res.end();
+}
