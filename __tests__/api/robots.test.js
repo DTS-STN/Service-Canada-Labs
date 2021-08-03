370f8bba02612e307bb97370cb4d256975d676fb
var-kyle <kyle.meelker@gmail.com>
2021-08-03 09:47:48 -0400

    add unit tests

---
+++ __tests__/api/robots.test.js
@@ -0,0 +1,27 @@
+import { createMocks } from "node-mocks-http";
+import handler from "../../pages/api/robots";
+import validateHandler from "../../pages/api/validate";
+
+describe("robots api", () => {
+  afterEach(async () => {
+    delete process.env.ENVIRONMENT;
+  });
+
+  it("renders the development robots.txt", async () => {
+    process.env.ENVIRONMENT = "development";
+    const { req, res } = createMocks({
+      method: "GET",
+    });
+    await handler(req, res);
+    expect(res._getData()).toBe("User-agent: *\nDisallow: /\n");
+  });
+
+  it("renders the production robots.txt", async () => {
+    process.env.ENVIRONMENT = "production";
+    const { req, res } = createMocks({
+      method: "GET",
+    });
+    await handler(req, res);
+    expect(res._getData()).toBe("User-agent: *\nDisallow: /api/*\n");
+  });
+});
