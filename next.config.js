99ef4efd2c51fe3f04c7c70c7fb50c495722c502
Kyle Meelker <34345435+var-kyle@users.noreply.github.com>
2021-08-03 11:06:09 -0400

    Add robots.txt (#321)
    * add robots api route and rewrite config
    
    * update changelog
    
    * add unit tests

--- next.config.js
+++ next.config.js
@@ -1,31 +1,31 @@
-const { i18n } = require('./next-i18next.config')
+const { i18n } = require("./next-i18next.config");
 
 securityHeaders = [
   // Only ever use HTTPS
   {
-    key: 'Strict-Transport-Security',
-    value: 'max-age=31536000; includeSubDomains; preload'
+    key: "Strict-Transport-Security",
+    value: "max-age=31536000; includeSubDomains; preload",
   },
   // Disables use of inline javascript in XSS attacks
   {
-    key: 'X-XSS-Protection',
-    value: '1; mode=block'
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
   },
   // Prevents the browser from attempting to guess the type of content
   {
-    key: 'X-Content-Type-Options',
-    value: 'nosniff'
+    key: "X-Content-Type-Options",
+    value: "nosniff",
   },
   // Only allow secure origin to be delivered over HTTPS
   {
-    key: 'Referrer-Policy',
-    value: 'strict-origin'
+    key: "Referrer-Policy",
+    value: "strict-origin",
   },
   {
-    key: 'Content-Security-Policy',
-    value: `frame-ancestors 'self'`
-  }
-]
+    key: "Content-Security-Policy",
+    value: `frame-ancestors 'self'`,
+  },
+];
 
 module.exports = {
   i18n,
@@ -37,9 +37,17 @@ module.exports = {
     return [
       {
         // Apply these headers to all routes in your application.
-        source: '/:path*{/}?',
+        source: "/:path*{/}?",
         headers: securityHeaders,
       },
-    ]
-  }
-}
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/robots.txt",
+        destination: "/api/robots",
+      },
+    ];
+  },
+};
