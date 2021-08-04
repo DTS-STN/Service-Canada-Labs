5d54499f99472020c2f00e3ec6078dd4ed5e8384
Jordan Willis <31868510+will0684@users.noreply.github.com>
2021-08-04 14:34:38 -0400

    remove x-powered-by header (#329)

--- next.config.js
+++ next.config.js
@@ -33,6 +33,7 @@ module.exports = {
     NEXT_PUBLIC_BUILD_DATE: process.env.NEXT_PUBLIC_BUILD_DATE,
     NEXT_PUBLIC_TC_BUILD: process.env.NEXT_PUBLIC_TC_BUILD,
   },
+  poweredByHeader: false,
   async headers() {
     return [
       {
