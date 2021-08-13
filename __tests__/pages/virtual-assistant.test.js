2ed6bf39bd28633c166240ae367db453337d64d5
shewood <28784886+shewood@users.noreply.github.com>
2021-08-13 14:11:59 -0400

    Vcon 293 demolink (#340)
    * updated the layout and text for VA
    
    * added button with link and test
    
    * removed a few empty lines in fr
    
    * removed the call to action section
    
    * updated the va link
    
    * fixed id on scenario

---
+++ __tests__/pages/virtual-assistant.test.js
@@ -0,0 +1,12 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, screen } from "@testing-library/react";
+import VirtualAssitant from "../../pages/projects/virtual-assistant";
+
+describe("Virtual Assitant Prototype page", () => {
+  it("renders without crashing", () => {
+    render(<VirtualAssitant />);
+    expect(screen.getByText("virtualAssistantTitle")).toBeInTheDocument();
+  });
+});
--- pages/projects/virtual-assistant/index.js
+++ pages/projects/virtual-assistant/index.js
@@ -3,9 +3,12 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
 import { Layout } from "../../../components/organisms/Layout";
 import { useRouter } from "next/router";
-import { VirtualConcierge } from "../../../components/organisms/VirtualConcierge";
-import { CallToAction } from "../../../components/molecules/CallToAction";
+import { ActionButton } from "../../../components//atoms/ActionButton";
 import { useEffect } from "react";
+
+//  On hold for now
+//  import { VirtualConcierge } from "../../../components/organisms/VirtualConcierge";
+// import { CallToAction } from "../../../components/molecules/CallToAction";
 
 export default function Home(props) {
   const { t } = useTranslation(["common", "vc"]);
@@ -55,84 +58,61 @@ export default function Home(props) {
           <meta name="dcterms.issued" content="2021-07-20" />
         </Head>
 
+        {/* Virtual Assitant Demo section start -  with link to working prototype */}
         <section className="layout-container mb-10">
-          <div className="w-auto mb-6">
-            <h1 className="mb-8 text-h1l" tabIndex="-1" id="pageMainTitle">
-              {t("vc:virtualAssistantTitle")}
-            </h1>
-            <div className="flex flex-col-reverse lg:grid lg:grid-cols-3 gap-4 lg:gap-6 ">
-              <p className="lg:col-span-2 px-1 lg:px-0 ">
-                {t("vc:virtualAssistantBio")}
-              </p>
+          <h1 className="mb-8 text-h1l" tabIndex="-1" id="pageMainTitle">
+            {t("vc:virtualAssistantTitle")}
+          </h1>
+          {/* the scenario section */}
+          <div className="whitespace-pre-line ">
+            <h2 className="mb-6 mt-8 text-h1" id="virtualAssistantScenario">
+              {t("vc:scenarioTitle")}
+            </h2>
+            <p
+              className="mb-6 lg:col-span-2 px-1 lg:px-0 xl:w-3/4"
+              dangerouslySetInnerHTML={{ __html: t("vc:scenarioBody") }}
+            >
+              {/* {t("vc:scenarioDemoBody")} */}
+            </p>
+            <p className="flex mb-16 text-center">
+              <ActionButton
+                href="https://av-va.alpha.service.canada.ca"
+                id="meet-va-link"
+                dataCy="meet-va-link"
+                className="rounded px-6 py-4 font-bold text-center inline-block"
+              >
+                {t("vc:meetAssistant")}
+              </ActionButton>
+            </p>
+          </div>
 
-              <div className=" bg-gradient-to-tr from-custom-blue-blue to-vc-blue-lt  p-4 ">
+          <div className="w-auto mb-6 ">
+            <div className="flex flex-col break-words lg:grid lg:grid-cols-2 gap-4 lg:gap-6 ">
+              <h2 className="mb-0 text-h1" id="virtualAssistantTitle">
+                {t("vc:virtualAssistantBioTitle")}
+              </h2>
+
+              <div className="row-span-2 bg-gradient-to-tr from-custom-blue-blue to-vc-blue-lt  p-4 ">
                 <img
                   className=" object-fill w-64 h-auto mx-auto"
                   src="/virtualconcierge/VirtualConcierge.svg"
                   alt={t("vc:virtualAssistantLogoAlt")}
                 />
               </div>
+              <p className=" font-body text-lg px-1 lg:px-0 ">
+                {t("vc:virtualAssistantBioBody")}
+              </p>
             </div>
           </div>
-          {/* the scenario section */}
-          <div>
-            <h2 className="mb-6 mt-8 text-h1" id="virtualAssistantTitle">
-              {t("vc:sectionTitle")}
-            </h2>
-            <VirtualConcierge
-              dataTestId="scenario1"
-              dataCy="scenario1"
-              description={t("vc:scenarioPart1")}
-              description1={t("vc:scenarioPart1-1")}
-              className=" bg-gray-200"
-              vcImage={
-                props.locale === "fr"
-                  ? "/virtualconcierge/fr/virtual-concierge1.png"
-                  : "/virtualconcierge/en/virtual-concierge1.png"
-              }
-              vcImageAltText={t("vc:altTextfigure1")}
-              figureCaption={t("vc:figureCaption1")}
-            />
-
-            <VirtualConcierge
-              dataTestId="scenario2"
-              dataCy="scenario2"
-              description={t("vc:scenarioPart2")}
-              className=" bg-gray-200"
-              vcImage={
-                props.locale === "fr"
-                  ? "/virtualconcierge/fr/virtual-concierge2.png"
-                  : "/virtualconcierge/en/virtual-concierge2.png"
-              }
-              vcImageAltText={t("vc:altTextfigure2")}
-              figureCaption={t("vc:figureCaption2")}
-            />
-            <VirtualConcierge
-              dataTestId="scenario3"
-              dataCy="scenario3"
-              description={t("vc:scenarioPart3")}
-              className=" bg-gray-200"
-              vcImage={
-                props.locale === "fr"
-                  ? "/virtualconcierge/fr/virtual-concierge3.png"
-                  : "/virtualconcierge/en/virtual-concierge3.png"
-              }
-              vcImageAltText={t("vc:altTextfigure3")}
-              figureCaption={t("vc:figureCaption3")}
-            />
-            <p className="pb-2 xl:w-3/4">{t("vc:scenarioPart4")}</p>
-            <p className="pb-2 xl:w-3/4">{t("vc:scenarioPart4-1")}</p>
-            <p className="my-6">
-              <strong>{t("projectsDisclaimer")}</strong>
-            </p>
-          </div>
         </section>
-        <CallToAction
+        {/* END Virtual Assistant Demo section end for working prototype */}
+
+        {/* <CallToAction
           title={t("signupTitleCallToAction")}
           html={t("becomeAParticipantDescription")}
           href={"/signup"}
           hrefText={t("signupBtn")}
-        />
+        /> */}
       </Layout>
       {process.env.NEXT_PUBLIC_ADOBE_ANALYTICS_URL ? (
         <script type="text/javascript">_satellite.pageBottom()</script>
