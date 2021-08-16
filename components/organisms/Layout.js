b2c62eb48bc54ef680b3dce3b14e9dbcb6540b70
EphraimAssi <1868295@cegepoutaouais.qc.ca>
2021-08-16 17:05:54 -0400

    Revert layout

--- components/organisms/Layout.js
+++ components/organisms/Layout.js
@@ -44,7 +44,10 @@ export const Layout = ({
         </a>
       </nav>
       <header>
-        <PhaseBanner phase={t("phaseBannerTag")} feedbackActive={true}>
+        <PhaseBanner
+          phase={t("phaseBannerTag")}
+          feedbackActive={feedbackActive}
+        >
           {t("phaseBannerText")}
         </PhaseBanner>
         <div className="layout-container flex-col flex lg:flex lg:flex-row justify-between  mt-2">
