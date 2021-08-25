b7a93605b117b19bde2536e2f8b20ed45176474b
Kyle Meelker <kyle.meelker@gmail.com>
2021-08-25 11:01:23 -0400

    add isr feature flag to easily enable or disable it

--- pages/projects.js
+++ pages/projects.js
@@ -166,13 +166,24 @@ export const getStaticProps = async ({ locale }) => {
     )
   );
 
-  return {
-    props: {
-      locale: locale,
-      ...(await serverSideTranslations(locale, ["common"])),
-      experimentData: data,
-      filters,
-    },
-    revalidate: 60 * 60 * 24, // revalidate once a day
-  };
+  const returnObj = process.env.ISR_ENABLED
+    ? {
+        props: {
+          locale: locale,
+          ...(await serverSideTranslations(locale, ["common"])),
+          experimentData: data,
+          filters,
+        },
+        revalidate: 60 * 60, // revalidate once an hour
+      }
+    : {
+        props: {
+          locale: locale,
+          ...(await serverSideTranslations(locale, ["common"])),
+          experimentData: data,
+          filters,
+        },
+      };
+
+  return returnObj;
 };
