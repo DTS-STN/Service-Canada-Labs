0b41b0206e30817353da877b52b6b5aa99fbe38b
shawn320 <azuneko1110@gmail.com>
2021-09-14 11:10:25 -0400

    test

--- pages/thankyou.js
+++ pages/thankyou.js
@@ -8,17 +8,17 @@ import { useEffect } from "react";
 export default function Confirmation(props) {
   const { t } = useTranslation("common");
   const { query } = useRouter();
-  const router = useRouter();
+  // const router = useRouter();
   const maskedEmail = String(query.e);
   const referrer = query.ref || "";
 
   useEffect(() => {
-    if (props.locale === "fr") {
-      router.push("/merci");
-    }
-    if (props.locale === "en") {
-      router.push("/thankyou");
-    }
+    // if (props.locale === "fr") {
+    //   router.push("/merci");
+    // }
+    // if (props.locale === "en") {
+    //   router.push("/thankyou");
+    // }
     if (process.env.NEXT_PUBLIC_ADOBE_ANALYTICS_URL) {
       window.adobeDataLayer = window.adobeDataLayer || [];
       window.adobeDataLayer.push({ event: "pageLoad" });
