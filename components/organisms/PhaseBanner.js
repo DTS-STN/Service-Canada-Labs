0a4565e376e70d0c42f67b6820a6f6c81167f14b
EphraimAssi <72703030+EphraimAssi@users.noreply.github.com>
2021-10-18 12:09:34 -0400

    Descriptive label for close button (#398)
    * Descriptive label for close button
    
    * Merge main
    
    * Revert layout

--- components/organisms/PhaseBanner.js
+++ components/organisms/PhaseBanner.js
@@ -194,13 +194,13 @@ export const PhaseBanner = ({ phase, children, feedbackActive }) => {
                   onClick={toggleForm}
                   className="font-body text-white flex mt-2.5 lg:mt-0 outline-none focus:outline-white-solid"
                   data-testid="closeButton"
+                  aria-label="Close the expanded feedback section"
                 >
-                  <span
-                    id="close"
-                    className="text-h2 lg:text-h1 leading-4 lg:leading-10"
-                  >
-                    &times;
-                  </span>
+                  <img
+                    src="/close-x.svg"
+                    alt="Close button"
+                    className="mt-0.5 lg:mt-3.5"
+                  />
                   <span className="text-xs leading-4 lg:text-sm underline ml-1 lg:ml-2 lg:leading-10">
                     {t("close")}
                   </span>
@@ -219,13 +219,13 @@ export const PhaseBanner = ({ phase, children, feedbackActive }) => {
                 onClick={toggleForm}
                 className="flex float-right pt-4 font-body text-white flex mt-2.5 lg:mt-0 outline-none focus:outline-white-solid"
                 data-testid="closeButton"
+                aria-label="Close the expanded feedback section"
               >
-                <span
-                  id="close"
-                  className="text-h3 lg:text-h2 leading-4 lg:leading-10"
-                >
-                  &times;
-                </span>
+                <img
+                  src="/close-x.svg"
+                  alt="Close button"
+                  className="mt-0.5 lg:mt-3.5"
+                />
                 <span className="text-xs leading-4 lg:text-sm underline ml-2 lg:leading-10">
                   {t("close")}
                 </span>
