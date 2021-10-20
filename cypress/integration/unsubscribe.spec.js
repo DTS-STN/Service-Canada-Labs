8dea866a323d19395753bd48496843c2c510c295
shawn320 <azuneko1110@gmail.com>
2021-10-20 09:00:41 -0400

    uncomment tests

--- cypress/integration/unsubscribe.spec.js
+++ cypress/integration/unsubscribe.spec.js
@@ -9,33 +9,33 @@ describe("unsubscribe page", () => {
     cy.injectAxe();
   });
 
-  // it("unsubscribe page loaded", () => {
-  //   cy.url().should("contains", "/unsubscribe");
-  // });
+  it("unsubscribe page loaded", () => {
+    cy.url().should("contains", "/unsubscribe");
+  });
 
-  // it("Has no detectable a11y violations on load", () => {
-  //   cy.checkA11y(null, null, terminalLog);
-  // });
+  it("Has no detectable a11y violations on load", () => {
+    cy.checkA11y(null, null, terminalLog);
+  });
 
-  // it("Adds subpath for french pages", () => {
-  //   cy.get('[data-cy="toggle-language-link"]').click();
-  //   cy.url().should("eq", "http://localhost:3000/fr/desabonnement");
-  // });
+  it("Adds subpath for french pages", () => {
+    cy.get('[data-cy="toggle-language-link"]').click();
+    cy.url().should("eq", "http://localhost:3000/fr/desabonnement");
+  });
 
-  // it("Fails to submit with no input", () => {
-  //   cy.get('[data-cy="unsubscribe-submit"]').click();
-  //   cy.url().should("contains", "/unsubscribe");
-  //   cy.get('[data-cy="error-box"').should("exist");
-  //   cy.get('[data-cy="error-box-items"').children().should("have.length", 1);
-  // });
+  it("Fails to submit with no input", () => {
+    cy.get('[data-cy="unsubscribe-submit"]').click();
+    cy.url().should("contains", "/unsubscribe");
+    cy.get('[data-cy="error-box"').should("exist");
+    cy.get('[data-cy="error-box-items"').children().should("have.length", 1);
+  });
 
-  // it("Validates that an email is entered in the email field", () => {
-  //   cy.get('[id="email"]').type("not an email");
+  it("Validates that an email is entered in the email field", () => {
+    cy.get('[id="email"]').type("not an email");
 
-  //   cy.get('[data-cy="unsubscribe-submit"]').click();
-  //   cy.get('[data-cy="error-box"').should("exist");
-  //   cy.get('[data-cy="error-box-items"').children().should("have.length", 1);
-  // });
+    cy.get('[data-cy="unsubscribe-submit"]').click();
+    cy.get('[data-cy="error-box"').should("exist");
+    cy.get('[data-cy="error-box-items"').children().should("have.length", 1);
+  });
 
   it("Test unsubscribe on successful submit", () => {
     cy.intercept("/api/**", { fixture: "signup" }).as("response");
