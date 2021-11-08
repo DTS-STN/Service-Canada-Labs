475bd6a3d3b073b5ee99a00b8c8d9d6ddaeb27f9
azuneko <azuneko1110@gmail.com>
2021-11-08 10:10:23 -0500

    Feedback popup (#410)
    * popup feedback
    
    * bottom feedback
    
    * update changelog
    
    * feedback active false
    
    * feedback unit test
    
    * update
    
    * new design on submit feedback
    
    * fix unit test
    
    * Reuse the FeedbackWidget component in PhaseBanner
    
    * change requests
    
    * untrack
    
    * some css update
    
    * remove package-lock
    
    * use focus trap to lock keyboard focus to feedback component
    
    * style issue
    
    * update
    
    * css issues, initial feedback error when close feedback
    
    * reset error when close feedback
    
    * change hard coded email address
    
    * add aria label to anchor tag

---
+++ components/molecules/FeedbackWidget.test.js
@@ -0,0 +1,39 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { axe, toHaveNoViolations } from "jest-axe";
+import { Primary } from "./FeedbackWidget.stories";
+
+expect.extend(toHaveNoViolations);
+
+describe("FeedbackWidget tests", () => {
+  it("renders FeedbackWidget in its primary state", () => {
+    render(<Primary {...Primary.args} />);
+    expect(screen.getByTestId("feedbackDropdown")).toHaveClass(
+      "w-full bg-custom-blue-blue"
+    );
+  });
+
+  it("no accessibility issues for form", async () => {
+    const { container } = render(<Primary {...Primary.args} />);
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+
+  it("no accessibility issues for thank you message", async () => {
+    const { container } = render(<Primary {...Primary.args} />);
+    const submitButton = screen.getByTestId("feedback-submit");
+    submitButton.click();
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+
+  it("has no a11y violations", async () => {
+    const { container } = render(<Primary {...Primary.args} />);
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
+  });
+});
