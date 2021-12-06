aa551a78bef74f7bb68216bf0b0ebea564aeba37
Kris Charbonneau <char0799@algonquinlive.com>
2021-12-06 11:32:19 -0500

    removed unused code as per LGTM

--- pages/projects/digital-centre.js
+++ pages/projects/digital-centre.js
@@ -5,7 +5,7 @@ import { useTranslation } from "next-i18next";
 import { HTMList } from "../../components/atoms/HTMList";
 import { Layout } from "../../components/organisms/Layout";
 import { CallToAction } from "../../components/molecules/CallToAction";
-import { useEffect, useRef, useState } from "react";
+import { useEffect } from "react";
 
 function ThumbnailWithCaption({
   title = "Image 1",
@@ -32,8 +32,6 @@ ThumbnailWithCaption.propTypes = {
 
 export default function DigitalCenter(props) {
   const { t } = useTranslation(["common", "dc"]);
-  const [showFeedback, setShowFeedback] = useState(false);
-  const toggle = useRef("Collapsed");
 
   useEffect(() => {
     if (process.env.NEXT_PUBLIC_ADOBE_ANALYTICS_URL) {
@@ -41,34 +39,6 @@ export default function DigitalCenter(props) {
       window.adobeDataLayer.push({ event: "pageLoad" });
     }
   }, []);
-
-  let toggleForm = async (e) => {
-    if (showFeedback) {
-      toggle.current = "Collapsed";
-    } else {
-      toggle.current = "Expanded";
-    }
-
-    srSpeak(toggle.current);
-    setShowFeedback(!showFeedback);
-  };
-
-  function srSpeak(text, priority) {
-    var el = document.createElement("div");
-    var id = "speak-" + Date.now();
-    el.setAttribute("id", id);
-    el.setAttribute("aria-live", priority || "polite");
-    el.classList.add("sr-only");
-    document.body.appendChild(el);
-
-    window.setTimeout(function () {
-      document.getElementById(id).innerHTML = text;
-    }, 100);
-
-    window.setTimeout(function () {
-      document.body.removeChild(document.getElementById(id));
-    }, 1000);
-  }
 
   return (
     <>
