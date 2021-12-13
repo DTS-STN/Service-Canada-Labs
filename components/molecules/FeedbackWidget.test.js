ef0227f8d4af2e8c2caf0a41d8c9f316ec93fda5
shawn320 <azuneko1110@gmail.com>
2021-12-13 08:47:09 -0500

    update test

--- components/molecules/FeedbackWidget.test.js
+++ components/molecules/FeedbackWidget.test.js
@@ -12,7 +12,7 @@ describe("FeedbackWidget tests", () => {
   it("renders FeedbackWidget in its primary state", () => {
     render(<Primary {...Primary.args} />);
     expect(screen.getByTestId("feedbackDropdown")).toHaveClass(
-      "w-full bg-custom-blue-blue"
+      "md:w-4/5 m-auto bg-custom-blue-blue shadow-lg border-black border-4"
     );
   });
 
