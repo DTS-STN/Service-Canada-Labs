9ce387bddbcaae62137dea310f2db7385f8ce833
shawn320 <azuneko1110@gmail.com>
2022-01-05 13:09:28 -0500

    fix unit test

--- setupTests.js
+++ setupTests.js
@@ -1,38 +1,38 @@
 import "@testing-library/jest-dom/extend-expect";
-import i18n from 'i18next'
-import { initReactI18next } from 'react-i18next'
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import "jest-canvas-mock";
 
 // next useRouter() mock
 jest.mock("next/router", () => ({
   useRouter() {
-      return {
-          route: "/",
-          pathname: "",
-          query: "",
-          asPath: "",
-      };
+    return {
+      route: "/",
+      pathname: "",
+      query: "",
+      asPath: "",
+    };
   },
 }));
 
 // Mock t function
 export const t = (key, params) => {
-  if (key === 'key.with.params') {
-    return `key.with.params.${params.param}`
+  if (key === "key.with.params") {
+    return `key.with.params.${params.param}`;
   }
 
-  return key
-}
-
+  return key;
+};
 
 // Mock react-i18next
 i18n.use(initReactI18next).init({
-  lng: 'en',
-  fallbackLng: 'en',
-  ns: ['common'],
-  defaultNS: 'common',
+  lng: "en",
+  fallbackLng: "en",
+  ns: ["common"],
+  defaultNS: "common",
   resources: {
     en: {
-      common: {}
-    }
-  }
-})
+      common: {},
+    },
+  },
+});
