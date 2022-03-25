64d64e1c6edba4bee4c97adecbfc3990de97ca91
shawn320 <azuneko1110@gmail.com>
2022-03-25 13:18:20 -0400

    spacing

--- components/molecules/Menu.js
+++ components/molecules/Menu.js
@@ -67,7 +67,7 @@ export function Menu(props) {
           return (
             <li
               key={key}
-              className={`py-3 lg:py-0 cursor-pointer text-custom-blue-projects-link list-none -my-4 -ml-4`}
+              className={`py-3 lg:py-0 cursor-pointer text-custom-blue-projects-link list-none -my-2 -ml-2`}
               role="menuitem"
               aria-current={exactURL ? "page" : null}
             >
