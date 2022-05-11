ba4f677f747c81d42914767cfc327fadf2ba8e3c
shawn320 <azuneko1110@gmail.com>
2022-05-11 13:31:38 -0400

    home, about, signup-info page no longer grabbing data from Strapi

--- __tests__/pages/about.test.js
+++ __tests__/pages/about.test.js
@@ -1,27 +1,12 @@
 /**
  * @jest-environment jsdom
  */
-import { screen } from "@testing-library/react";
-import fetchMock from "fetch-mock";
+import { render, screen } from "@testing-library/react";
+import About from "../../pages/about";
 
 describe("About", () => {
-  beforeEach(() => {
-    fetchMock.getOnce(
-      `${process.env.NEXT_PUBLIC_STRAPI_API_BACKEND_URL}/page-contents?populate=%2A&locale=all`,
-      {
-        status: 200,
-        body: "page contents",
-      }
-    );
-  });
-  afterEach(() => {
-    fetchMock.restore();
-  });
-  it.skip("renders without crashing", async () => {
-    const { render } = await getPage({
-      route: "/about",
-    });
-    render();
-    expect(screen.getAllByText("About these labs")[0]).toBeInTheDocument();
+  it("renders without crashing", () => {
+    render(<About />);
+    expect(screen.getByText("skipToMainContentBtn")).toBeInTheDocument();
   });
 });
--- __tests__/pages/signup-info.test.js
+++ __tests__/pages/signup-info.test.js
@@ -1,29 +1,12 @@
 /**
  * @jest-environment jsdom
  */
-import { screen } from "@testing-library/react";
-import fetchMock from "fetch-mock";
+import { render, screen } from "@testing-library/react";
+import SignupInfo from "../../pages/signup-info";
 
 describe("About", () => {
-  beforeEach(() => {
-    fetchMock.getOnce(
-      `${process.env.NEXT_PUBLIC_STRAPI_API_BACKEND_URL}/page-contents?populate=%2A&locale=all`,
-      {
-        status: 200,
-        body: "signup info page content",
-      }
-    );
-  });
-  afterEach(() => {
-    fetchMock.restore();
-  });
-  it.skip("renders without crashing", async () => {
-    const { render } = await getPage({
-      route: "/signup-info",
-    });
-    render();
-    expect(
-      screen.getAllByText("Sign up to be a voice in tomorrow's services")[0]
-    ).toBeInTheDocument();
+  it("renders without crashing", () => {
+    render(<SignupInfo />);
+    expect(screen.getByText("skipToMainContentBtn")).toBeInTheDocument();
   });
 });
