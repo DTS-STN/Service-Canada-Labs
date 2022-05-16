c587d0d441c7d8dabc91a4e949f9412d3c1bc86b
azuneko <azuneko1110@gmail.com>
2022-05-16 11:29:15 -0400

    home, about, signup-info page no longer grabbing data from Strapi (#501)

--- __tests__/pages/home.test.js
+++ __tests__/pages/home.test.js
@@ -1,27 +1,12 @@
 /**
  * @jest-environment jsdom
  */
-import { screen } from "@testing-library/react";
-import fetchMock from "fetch-mock";
+import { render, screen } from "@testing-library/react";
+import Home from "../../pages/home";
 
-describe("Homepage content", () => {
-  beforeEach(() => {
-    fetchMock.getOnce(
-      `${process.env.NEXT_PUBLIC_STRAPI_API_BACKEND_URL}/page-contents?populate=%2A&locale=all`,
-      {
-        status: 200,
-        body: "Homepage content",
-      }
-    );
-  });
-  afterEach(() => {
-    fetchMock.restore();
-  });
-  it.skip("renders without crashing", async () => {
-    const { render } = await getPage({
-      route: "/home",
-    });
-    render();
-    expect(screen.getAllByText("Service Cananda Labs")[0]).toBeInTheDocument();
+describe("Home", () => {
+  it("renders without crashing", () => {
+    render(<Home />);
+    expect(screen.getByText("projectsAndExplorationTitle")).toBeInTheDocument();
   });
 });
