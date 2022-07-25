b1dcdf1c6ea9a52310b0095dcf42a685f5377ae2
shawn320 <azuneko1110@gmail.com>
2022-07-25 15:00:29 -0400

    remove unused variable

--- pages/projects/life-journeys.js
+++ pages/projects/life-journeys.js
@@ -2,7 +2,6 @@ import { Layout } from "../../components/organisms/Layout";
 import Head from "next/head";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
-import { useRouter } from "next/router";
 import { CallToAction } from "../../components/molecules/CallToAction";
 import { useEffect, useState } from "react";
 import queryGraphQL from "../../graphql/client";
