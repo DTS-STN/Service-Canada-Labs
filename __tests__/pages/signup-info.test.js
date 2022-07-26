0823f5dcdc0303fc6f0cce216a1ea35ee38844a8
shawn320 <azuneko1110@gmail.com>
2022-07-26 13:42:28 -0400

    update signup-info

--- __tests__/pages/signup-info.test.js
+++ __tests__/pages/signup-info.test.js
@@ -3,10 +3,17 @@
  */
 import { render, screen } from "@testing-library/react";
 import SignupInfo from "../../pages/signup-info";
+import { signupInfoPageData } from "../../__mocks__/mockStore";
 
-describe("About", () => {
+jest.mock("@apollo/client");
+
+describe("Signup Info", () => {
   it("renders without crashing", () => {
-    render(<SignupInfo />);
-    expect(screen.getByText("skipToMainContentBtn")).toBeInTheDocument();
+    render(<SignupInfo pageData={signupInfoPageData.data.sCLabsPageByPath} />);
+    expect(
+      screen.getByRole("heading", {
+        name: "S’inscrire pour participer à l’élaboration des services de demain",
+      })
+    ).toBeInTheDocument();
   });
 });
