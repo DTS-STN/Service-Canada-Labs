d09b6e54dd44017199a23a5b55587fda0491c61d
Zain Syed <zeddotes@gmail.com>
2022-09-14 15:25:16 -0400

    - updated revalidation error stuff

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,25 +1,5 @@
 import aemServiceInstance from "../../services/aemServiceInstance";
 
-const getPromise = async (promise) => {
-  return Promise.resolve().then(() => promise);
-};
-
-const callTasks = async (promises) => {
-  try {
-    const promise = await getPromise(promises.shift());
-
-    if (!promises.length) {
-      return promise;
-    }
-
-    return callTasks(promises);
-  } catch (err) {
-    console.error(err.message);
-
-    return promise;
-  }
-};
-
 export default async function handler(req, res) {
   // Check for secret to confirm this is a valid request
   if (req.query.secret !== process.env.REVALIDATION_TOKEN) {
@@ -28,18 +8,22 @@ export default async function handler(req, res) {
 
   const routes = req.query?.routes?.split(",") || [];
 
-  if (!routes?.length) {
-    return res.status(500).send("Pass in routes to revalidate");
+  if (!routes?.length || !routes.every((route) => route.startsWith("/"))) {
+    return res
+      .status(500)
+      .send(
+        "Pass in valid routes to revalidate, separated by a comma and no whitespaces"
+      );
   }
 
   try {
     aemServiceInstance.flush();
-    await callTasks(routes.map((route) => res.revalidate(route)));
+    await Promise.all(routes.map((route) => res.revalidate(route)));
     return res.json({ revalidated: true });
   } catch (err) {
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
     console.log(err);
-    return res.status(500).send("Error revalidating");
+    return res.status(500).json({});
   }
 }
