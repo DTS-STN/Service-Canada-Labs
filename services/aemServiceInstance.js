14d0772dd4b68e9b89719d5cb67fd3bf525c26c3
Zain Syed <zeddotes@gmail.com>
2022-09-15 14:55:14 -0400

    - removed condition for test env

--- services/aemServiceInstance.js
+++ services/aemServiceInstance.js
@@ -1,8 +1,8 @@
 import AEMService from './AEMService'
 
-const aemServiceInstance = process.env.NODE_ENV !== 'test' ? new AEMService(
+const aemServiceInstance = new AEMService(
   process.env.AEM_GRAPHQL_ENDPOINT,
   process.env.BUILD_DATE
-) : {}
+)
 
 export default aemServiceInstance
