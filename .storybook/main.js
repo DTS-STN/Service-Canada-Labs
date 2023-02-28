ea5bd6b60dc524497af9702b12e3acc476fbec7c
Jordan <jordan.walcaraz@gmail.com>
2023-02-27 18:32:55 -0800

    update storybook to 6.5.16, migrate to webpack 5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,33 +1,25 @@
 var webpack = require("webpack");
 var path = require("path");
-
 module.exports = {
-  stories: [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-    "../components/**/*/*.stories.@(js|jsx|ts|tsx)",
-  ],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-a11y",
-    {
-      name: "@storybook/addon-postcss",
-      options: {
-        postcssLoaderOptions: {
-          implementation: require("postcss"),
-        },
-      },
-    },
-    "storybook-addon-next-router",
-  ],
-  webpackFinal: (config) => {
+  stories: ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)", "../components/**/*/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-a11y", {
+    name: "@storybook/addon-postcss",
+    options: {
+      postcssLoaderOptions: {
+        implementation: require("postcss")
+      }
+    }
+  }, "storybook-addon-next-router"],
+  webpackFinal: config => {
     // useTranslation() hook in next-i18next is looking for a server environment and storybooks
     // runs client-side, so use client-side react-i18next to allow components to render outside server environment
     config.resolve.alias = {
       ...config.resolve.alias,
-      "next-i18next": "react-i18next",
+      "next-i18next": "react-i18next"
     };
     return config;
   },
+  core: {
+    builder: "webpack5"
+  }
 };
