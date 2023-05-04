152f8980103d6d3dad5cb6909fa0f372c42f640e
Jordan Willis <31868510+will0684@users.noreply.github.com>
2023-05-04 10:37:27 -0700

    Card.js can be right-clicked to open a new tab (#746)
    * Card.js can be right-clicked to open a new tab
    
    * update CHANGELOG.md

--- components/molecules/Card.js
+++ components/molecules/Card.js
@@ -15,7 +15,7 @@ export const Card = (props) => {
   };
 
   return (
-    <Link href={props.href} legacyBehavior>
+    <Link href={props.href}>
       <div
         className={`group card-shadow border border-custom-gray-border rounded-md pb-4 hover:cursor-pointer ${
           "border-" + (tagColours[props.tag] || "gray-experiment")
