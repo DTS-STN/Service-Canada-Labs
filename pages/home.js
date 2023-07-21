47ef17413f8130695098efe454cf9ed29d3621b2
Jordan Willis <31868510+will0684@users.noreply.github.com>
2023-07-21 15:41:06 -0700

    Benefits Navigator Overview Page + Card on Home (#811)
    * add smaller size to tailwind max-width utilities
    
    * create Benefits Navigator query
    
    * rename mscadPageQuery query name
    
    * modify ul, li global margin style and li default font-size
    
    * remove unnecessary class from list items
    
    * create Benefits Navigator page, heading section and start of features section
    
    * create elements for Feature sections
    
    * modify margin on list and list items in text versions of images
    
    * modify global css for how sequential p, ul, ol, li elements manage spacing
    
    * added REWRITE for benefits-navigator french paths
    
    * formatting and updated projectQuery to newer fragment models
    
    * get Benefits Navigator card on homepage
    
    * remove unnecessary padding from sequential parapgraph elements now being applied globally
    
    * complete page minus updates section, add ordered list to recursive function
    
    * remove unecessary p padding now applied globally
    
    * more removal of padding applied globally
    
    * update CHANGELOG.md

--- pages/home.js
+++ pages/home.js
@@ -14,8 +14,7 @@ export default function Home(props) {
   const currentProjects = experimentsData.filter((project) => {
     return (
       project.scLabProjectStatus[0] ===
-        "gc:custom/decd-endc/project-status/current" &&
-      project.scId !== "BENEFITS-NAVIGATOR-OVERVIEW"
+      "gc:custom/decd-endc/project-status/current"
     );
   });
 
@@ -31,7 +30,12 @@ export default function Home(props) {
         tagLabel={props.locale === "en" ? "New Update" : "Nouvelle mise à jour"}
         tag="current_projects"
         imgSrc={
-          props.locale === "en"
+          // TODO images should always be fetched from the same place in the response data i.e. using the socialMediaImage field
+          project.scId === "BENEFITS-NAVIGATOR-OVERVIEW"
+            ? props.locale === "en"
+              ? `${project.scFragments[0].scImageEn._publishUrl}`
+              : `${project.scFragments[0].scImageFr._publishUrl}`
+            : props.locale === "en"
             ? `https://www.canada.ca${project.scSocialMediaImageEn._path}`
             : `https://www.canada.ca${project.scSocialMediaImageFr._path}`
         }
