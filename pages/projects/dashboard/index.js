0eaa9262d7062c2f077d9f43b1487939f14590b2
Frank <48450599+Fbasham@users.noreply.github.com>
2023-08-16 13:16:50 -0400

    [frontend] add locale prop to ProjectInfo component (#839)

--- pages/projects/dashboard/index.js
+++ pages/projects/dashboard/index.js
@@ -258,6 +258,7 @@ export default function MscaDashboard(props) {
               </p>
               <div className="row-start-3">
                 <ProjectInfo
+                  locale={props.locale}
                   termStarted={
                     props.locale === "en"
                       ? filteredDictionary[2].scTermEn
