759c822f7179eae7549694a3d5d8b7dd09e14d35
Jordan <jordan.walcaraz@gmail.com>
2023-12-05 14:48:12 -0500

    add check for scButtonType in FragmentRender

--- components/fragment_renderer/fragment_components/Button.js
+++ components/fragment_renderer/fragment_components/Button.js
@@ -1,11 +1,22 @@
 import { ActionButton } from "../../atoms/ActionButton";
 
 export default function Button(props) {
+  const style =
+    props.fragmentData.scButtonType === null
+      ? "primary"
+      : props.fragmentData.scButtonType[0] ===
+        "gc:custom/decd-endc/button-type/primary"
+      ? "primary"
+      : props.fragmentData.scButtonType[0] ===
+        "gc:custom/decd-endc/button-type/secondary"
+      ? "secondary"
+      : "primary";
+
   return (
     <div className="grid grid-cols-12 gap-x-6 my-12">
       <ActionButton
         id={props.key}
-        style="primary"
+        style={style}
         custom="col-span-12"
         href={
           props.locale === "en"
