a5b7673db1d28a4bc33a268df8524ef9a91e08eb
Jordan <jordan.walcaraz@gmail.com>
2024-01-09 12:22:11 -0500

    modify storybook webpack config to fix svg loader

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,7 @@
 var webpack = require("webpack");
 var path = require("path");
 module.exports = {
+  staticDirs: ["../public",],
   stories: [
     "../stories/**/*.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
@@ -13,6 +14,24 @@ module.exports = {
     "@storybook/addon-styling",
   ],
   webpackFinal: (config) => {
+    config.module = config.module || {};
+    config.module.rules = config.module.rules || [];
+
+    // This modifies the existing image rule to exclude .svg files
+    // since you want to handle those files with @svgr/webpack
+    const imageRule = config.module.rules.find((rule) =>
+      rule?.["test"]?.test(".svg")
+    );
+    if (imageRule) {
+      imageRule["exclude"] = /\.svg$/;
+    }
+
+    // Configure .svg files to be loaded with @svgr/webpack
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
     // useTranslation() hook in next-i18next is looking for a server environment and storybooks
     // runs client-side, so use client-side react-i18next to allow components to render outside server environment
     config.resolve.alias = {
