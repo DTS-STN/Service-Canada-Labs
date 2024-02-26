2c06c8cf58b198f6ffbd46ea10789a5f736fb9e2
Jordan <jordan.walcaraz@gmail.com>
2024-02-26 12:52:15 -0500

    remove unnecessary conditional from project page Cards imgHeight and imgWidth

--- pages/projects/benefits-navigator/index.js
+++ pages/projects/benefits-navigator/index.js
@@ -48,16 +48,8 @@ export default function BenefitsNavigatorOverview(props) {
             ? update.scSocialMediaImageAltTextEn
             : update.scSocialMediaImageAltTextFr
         }
-        imgHeight={
-          update.scSocialMediaImageEn.height
-            ? update.scSocialMediaImageEn.height
-            : ""
-        }
-        imgWidth={
-          update.scSocialMediaImageEn.width
-            ? update.scSocialMediaImageEn.width
-            : ""
-        }
+        imgHeight={update.scSocialMediaImageEn.height}
+        imgWidth={update.scSocialMediaImageEn.width}
         title={props.locale === "en" ? update.scTitleEn : update.scTitleFr}
         href={props.locale === "en" ? update.scPageNameEn : update.scPageNameFr}
         description={`${
