90ff114da5f5f7cce9be06e150b14addf52aff7d
blai0264 <mblais93@gmail.com>
2024-05-08 13:36:17 -0400

    added ImageVerticalLineContent story

---
+++ components/fragment_renderer/fragment_components/ImageVerticalLineContent.stories.js
@@ -0,0 +1,30 @@
+import * as React from "react";
+import ImageVerticalLineContent from "./ImageVerticalLineContent";
+
+export default {
+  title: "Components/Fragment_Renderer/ImageVerticalLineContent",
+  component: ImageVerticalLineContent,
+};
+
+const Template = (args) => <ImageVerticalLineContent {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  id: "ImageVerticalLineContent",
+  src: "/image2.png",
+  alt: "image alt text",
+  width: 359,
+  height: 260,
+  data: [
+    {
+      nodeType: "paragraph",
+      content: [
+        {
+          nodeType: "text",
+          value:
+            "Every week, our product team meets for Feedback Friday to sort through all the new survey responses. We look at the ratings and comments people shared with us about their experience. ",
+        },
+      ],
+    },
+  ],
+};
