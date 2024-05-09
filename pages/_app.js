bf898028b72cc856f7a714d29b3bd3de4e3c18bc
Jordan <jordan.walcaraz@gmail.com>
2024-05-09 14:04:08 -0400

    setup google fonts to be served locally

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,15 @@ import "../styles/menu.css";
 import Head from "next/head";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import { Noto_Sans, Lato } from "next/font/google";
 
 config.autoAddCss = false;
+
+const notoSans = Noto_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "700", "900"],
+});
+const lato = Lato({ subsets: ["latin"], weight: ["300", "400", "700", "900"] });
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -14,7 +21,9 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <Component {...pageProps} />
+      <main className={[notoSans.className, lato.className]}>
+        <Component {...pageProps} />
+      </main>
     </>
   );
 }
