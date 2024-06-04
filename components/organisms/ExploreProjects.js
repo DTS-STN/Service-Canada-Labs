0df51e82f5bba6d1d4f2dcd7c7151385d0b00e45
blai0264 <mblais93@gmail.com>
2024-06-04 17:42:24 -0400

    updated ExploreProjects component. Made it so entire section explore other projects section of html is generated inside component to increase reusability

--- components/organisms/ExploreProjects.js
+++ components/organisms/ExploreProjects.js
@@ -1,16 +1,12 @@
 import React from "react";
 import Card from "../molecules/Card";
 
-export function ExploreProjects(props) {
-  const { locale, activeProjectId, projects } = props;
-  console.log(projects);
-  const filteredProjects = projects.filter((currentProject) => {
-    return currentProject.scId !== activeProjectId;
-  });
-  if (filteredProjects.length > 3) filteredProjects.length = 3;
-  console.log(filteredProjects);
-  const projectCards = filteredProjects.map((project) => (
-    <li key={project.scId} className="list-none ml-0 col-span-12 lg:col-span-4">
+const createCard = (locale, project) => {
+  return (
+    <li
+      key={project.scId}
+      className="bg-multi-neutrals-white list-none ml-0 col-span-12 lg:col-span-4"
+    >
       <Card
         showImage
         imgSrc={
@@ -34,6 +30,27 @@ export function ExploreProjects(props) {
         }
       />
     </li>
-  ));
-  return projectCards;
+  );
+};
+
+export function ExploreProjects(props) {
+  const { locale, activeProjectId, projects } = props;
+  const filteredProjects = projects.filter((currentProject) => {
+    return currentProject.scId !== activeProjectId;
+  });
+  if (filteredProjects.length > 3) filteredProjects.length = 3;
+  return (
+    <div className="max-w-full bg-multi-blue-blue65b">
+      <div className="layout-container">
+        <h2 className="text-multi-neutrals-white">
+          {props.locale === "en"
+            ? "Explore other projects"
+            : "Explorer d'autres projets"}
+        </h2>
+      </div>
+      <ul className="layout-container grid lg:grid-cols-12 gap-x-10 lg:gap-y-4 list-none ml-0">
+        {filteredProjects.map((project) => createCard(locale, project))}
+      </ul>
+    </div>
+  );
 }
