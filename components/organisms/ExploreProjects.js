7caac295b512ab5932403398e77053ab929162ba
Jordan <jordan.walcaraz@gmail.com>
2024-06-04 14:03:40 -0400

    modify .map return in ExploreProjects.js

--- components/organisms/ExploreProjects.js
+++ components/organisms/ExploreProjects.js
@@ -9,36 +9,31 @@ export function ExploreProjects(props) {
   });
   if (filteredProjects.length > 3) filteredProjects.length = 3;
   console.log(filteredProjects);
-  const projectCards = filteredProjects.map((project) => {
-    return (
-      <li
-        key={project.scId}
-        className="list-none ml-0 col-span-12 lg:col-span-4"
-      >
-        <Card
-          showImage
-          imgSrc={
-            locale === "en"
-              ? `https://www.canada.ca${project.scSocialMediaImageEn?._path}`
-              : `https://www.canada.ca${project.scSocialMediaImageFr?._path}`
-          }
-          imgAlt={
-            locale === "en"
-              ? project.scSocialMediaImageAltTextEn
-              : project.scSocialMediaImageAltTextFr
-          }
-          imgHeight={project.scSocialMediaImageEn.height}
-          imgWidth={project.scSocialMediaImageEn.width}
-          title={locale === "en" ? project.scTitleEn : project.scTitleFr}
-          href={locale === "en" ? project.scPageNameEn : project.scPageNameFr}
-          description={
-            locale === "en"
-              ? project.scDescriptionEn.json[0].content[0].value
-              : project.scDescriptionFr.json[0].content[0].value
-          }
-        />
-      </li>
-    );
-  });
+  const projectCards = filteredProjects.map((project) => (
+    <li key={project.scId} className="list-none ml-0 col-span-12 lg:col-span-4">
+      <Card
+        showImage
+        imgSrc={
+          locale === "en"
+            ? `https://www.canada.ca${project.scSocialMediaImageEn?._path}`
+            : `https://www.canada.ca${project.scSocialMediaImageFr?._path}`
+        }
+        imgAlt={
+          locale === "en"
+            ? project.scSocialMediaImageAltTextEn
+            : project.scSocialMediaImageAltTextFr
+        }
+        imgHeight={project.scSocialMediaImageEn.height}
+        imgWidth={project.scSocialMediaImageEn.width}
+        title={locale === "en" ? project.scTitleEn : project.scTitleFr}
+        href={locale === "en" ? project.scPageNameEn : project.scPageNameFr}
+        description={
+          locale === "en"
+            ? project.scDescriptionEn.json[0].content[0].value
+            : project.scDescriptionFr.json[0].content[0].value
+        }
+      />
+    </li>
+  ));
   return projectCards;
 }
