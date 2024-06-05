08846c6ebfe9218326f1c9f313cc6ba7f1915e57
blai0264 <mblais93@gmail.com>
2024-06-05 19:01:29 -0400

    updates to ExploreProjects component, added dynamicH2 function which whandles changing the text content for mobile view, also updated styles to match designs

--- components/organisms/ExploreProjects.js
+++ components/organisms/ExploreProjects.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Card from "../molecules/Card";
+import { useState } from "react";
 
 //callback function that creates a card for a project when called
 const createCard = (locale, project) => {
   return (
     <li
       key={project.scId}
-      className="bg-multi-neutrals-white list-none ml-0 col-span-12 lg:col-span-4"
+      className="card-shadow rounded-md bg-multi-neutrals-white ml-0 col-span-12 lg:col-span-4"
     >
       <Card
         showImage
@@ -34,6 +35,29 @@ const createCard = (locale, project) => {
   );
 };
 
+const dynamicH2 = (locale) => {
+  const [isMobile, setMobile] = useState(false);
+  const updateMedia = () => {
+    window.innerWidth < 366 ? setMobile(true) : setMobile(false);
+  };
+  useEffect(() => {
+    updateMedia();
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  }, []);
+  return (
+    <h2 className="text-multi-neutrals-white">
+      {isMobile
+        ? locale === "en"
+          ? "Other projects"
+          : "D'autres projets"
+        : locale === "en"
+        ? "Explore other projects"
+        : "Explorer d'autres projets"}
+    </h2>
+  );
+};
+
 export function ExploreProjects(props) {
   //initialize props
   const { locale, activeProjectId, projects } = props;
@@ -45,15 +69,11 @@ export function ExploreProjects(props) {
   if (filteredProjects.length > 3) filteredProjects.length = 3;
   return (
     //create html for "Explore other projects" section
-    <div className="max-w-full bg-multi-blue-blue65b">
-      <div className="layout-container">
-        <h2 className="text-multi-neutrals-white">
-          {locale === "en"
-            ? "Explore other projects"
-            : "Explorer d'autres projets"}
-        </h2>
+    <div className="grid justify-evenly content-center lg:h-[740px] max-w-full bg-multi-blue-blue65b">
+      <div className="xxs:mt-20 lg:mt-30 layout-container">
+        {dynamicH2(locale)}
       </div>
-      <ul className="layout-container grid lg:grid-cols-12 gap-x-10 lg:gap-y-4 list-none ml-0">
+      <ul className="xs:mb-40 sm:mb-40 lg:mb-15 justify-self:center layout-container grid lg:grid-cols-12 gap-x-6 gap-y-6 list-none ml-0">
         {/*iterate over filteredProjects array and create card for each project */}
         {filteredProjects.map((project) => createCard(locale, project))}
       </ul>
