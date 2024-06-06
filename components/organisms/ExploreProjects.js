7e26f66da328d6aa9f2fc04a82731722257aa90a
blai0264 <mblais93@gmail.com>
2024-06-06 11:33:39 -0400

    stashing changes

--- components/organisms/ExploreProjects.js
+++ components/organisms/ExploreProjects.js
@@ -35,27 +35,15 @@ const createCard = (locale, project) => {
   );
 };
 
-const dynamicH2 = (locale) => {
-  const [isMobile, setMobile] = useState(false);
-  const updateMedia = () => {
-    window.innerWidth < 366 ? setMobile(true) : setMobile(false);
-  };
-  useEffect(() => {
-    updateMedia();
-    window.addEventListener("resize", updateMedia);
-    return () => window.removeEventListener("resize", updateMedia);
-  }, []);
-  return (
-    <h2 className="text-multi-neutrals-white">
-      {isMobile
-        ? locale === "en"
-          ? "Other projects"
-          : "D'autres projets"
-        : locale === "en"
-        ? "Explore other projects"
-        : "Explorer d'autres projets"}
-    </h2>
-  );
+const shuffleNSlice = (array) => {
+  //randomize order of array
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  //slice array to max length of 3
+  array = array.slice(0, 3);
+  return array;
 };
 
 export function ExploreProjects(props) {
@@ -65,17 +53,26 @@ export function ExploreProjects(props) {
   const filteredProjects = projects.filter((currentProject) => {
     return currentProject.scId !== activeProjectId;
   });
-  //sets max length of new array to 3
-  if (filteredProjects.length > 3) filteredProjects.length = 3;
+  //randomize order of filtered projects and store in shuffledProjects
+  const shuffledProjects = shuffleNSlice(filteredProjects);
+  console.log(shuffledProjects);
+
   return (
     //create html for "Explore other projects" section
     <div className="grid justify-evenly content-center lg:h-[740px] max-w-full bg-multi-blue-blue65b">
       <div className="xxs:mt-20 lg:mt-30 layout-container">
-        {dynamicH2(locale)}
+        <h2 className="lg-view text-multi-neutrals-white">
+          {locale === "en"
+            ? "Explore other projects"
+            : "Explorer d'autres projets"}
+        </h2>
+        <h2 className="sm-view text-multi-neutrals-white">
+          {locale === "en" ? "Other projects" : "D'autres projets"}
+        </h2>
       </div>
       <ul className="xs:mb-40 sm:mb-40 lg:mb-15 justify-self:center layout-container grid lg:grid-cols-12 gap-x-6 gap-y-6 list-none ml-0">
         {/*iterate over filteredProjects array and create card for each project */}
-        {filteredProjects.map((project) => createCard(locale, project))}
+        {shuffledProjects.map((project) => createCard(locale, project))}
       </ul>
     </div>
   );
