d2d9a5719c7c69e7221e0c875b221254cc98ef4d
Jordan <jordan.walcaraz@gmail.com>
2024-06-07 14:35:46 -0400

    update home.test.js

--- __tests__/pages/home.test.js
+++ __tests__/pages/home.test.js
@@ -5,13 +5,15 @@ import { render, screen } from "@testing-library/react";
 import Home from "../../pages/home";
 import { homePageData } from "../../__mocks__/mockStore";
 import { experimentsData } from "../../__mocks__/mockStore";
+import { dictionaryData } from "../../__mocks__/mockStore";
 
 describe("Home", () => {
   it("renders without crashing", () => {
     render(
       <Home
-        pageData={homePageData.data.scLabsPagev1ByPath}
+        pageData={homePageData.data.sclabsPageV1ByPath}
         experimentsData={experimentsData.data.scLabsPagev1List.items}
+        dictionary={dictionaryData.data.dictionaryV1List}
       />
     );
     expect(
