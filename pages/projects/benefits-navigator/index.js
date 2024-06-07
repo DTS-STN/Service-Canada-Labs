3fb76fa21fdfb084b11e3059331822b992f4063f
blai0264 <mblais93@gmail.com>
2024-06-07 11:01:31 -0400

    removed filterProjects and imported as a util

--- pages/projects/benefits-navigator/index.js
+++ pages/projects/benefits-navigator/index.js
@@ -14,6 +14,7 @@ import stageDictionary from "../../../lib/utils/stageDictionary";
 import { ExploreProjects } from "../../../components/organisms/ExploreProjects";
 import TextRender from "../../../components/text_node_renderer/TextRender";
 import { shuffle } from "../../../lib/utils/shuffle";
+import { filterProjects } from "../../../lib/utils/filterProjects";
 
 export default function BenefitsNavigatorOverview(props) {
   const [allProjects] = useState(props.allProjects);
@@ -55,16 +56,6 @@ export default function BenefitsNavigatorOverview(props) {
       />
     </li>
   ));
-
-  const filterProjects = (projects, activeProject) => {
-    //filter out current project from projects array
-    const filteredProjects = projects.filter((currentProject) => {
-      return currentProject.scId !== activeProject;
-    });
-    //slice filtered array to max length of 3 and return new array
-    const slicedArray = filteredProjects.slice(0, 3);
-    return slicedArray;
-  };
 
   useEffect(() => {
     if (props.adobeAnalyticsUrl) {
