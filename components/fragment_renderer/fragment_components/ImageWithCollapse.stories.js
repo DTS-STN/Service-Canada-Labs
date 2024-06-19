b5015dd83287de5cc1284b9bf71934904bcd8536
blai0264 <mblais93@gmail.com>
2024-06-19 14:31:40 -0400

    updated ImageWithCollapse story and unit test

--- components/fragment_renderer/fragment_components/ImageWithCollapse.stories.js
+++ components/fragment_renderer/fragment_components/ImageWithCollapse.stories.js
@@ -13,12 +13,11 @@ const Template = (args) => <ImageWithCollapse {...args} />;
 export const Default = Template.bind({});
 Default.args = {
   id: "ImageWithCollapse",
-  src: "/image2.png",
+  src: CollapseData.scImageEn._publishUrl,
   alt: "image alt text",
-  width: 359,
-  height: 260,
-  content:
-    "Every week, our product team meets for Feedback Friday to sort through all the new survey responses. We look at the ratings and comments people shared with us about their experience. ",
+  width: CollapseData.scImageEn.width,
+  height: CollapseData.scImageEn.height,
+  content: CollapseData.scImageCaptionEn.json[0].content[0].value,
   title: "Example Title",
   longDesc: "Test Description",
   children: <TextRender data={CollapseData.scLongDescEn.json} />,
--- components/fragment_renderer/fragment_components/ImageWithCollapse.test.js
+++ components/fragment_renderer/fragment_components/ImageWithCollapse.test.js
@@ -11,9 +11,7 @@ describe("ImageWithCollapse", () => {
   test("renders ImageWithCollapse component with default props", async () => {
     const { container } = render(<ImageWithCollapse {...Default.args} />);
     expect(screen.getByAltText("image alt text")).toBeInTheDocument();
-    expect(
-      screen.getByText((content) => content.startsWith("Every"))
-    ).toBeInTheDocument();
+    expect(screen.getByText("Figure 1")).toBeInTheDocument;
     expect(
       screen.getByText((content) => content.startsWith("Example Title"))
     ).toBeInTheDocument();
