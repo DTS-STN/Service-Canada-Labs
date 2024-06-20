367cb4350d8d1b1ea689026dc62152f176c3eccb
Jordan <jordan.walcaraz@gmail.com>
2024-06-20 15:17:17 -0400

    remove superfuous check in ProjectInfo.test.js

--- components/atoms/ProjectInfo.test.js
+++ components/atoms/ProjectInfo.test.js
@@ -15,11 +15,9 @@ describe("ProjectInfo tests", () => {
     const dateStarted = screen.getByText("2021-01-01");
     const dateEnded = screen.getByText("2022-03-31");
     const stage = screen.getByText("Alpha");
-    const status = screen.getByText("Project status");
     expect(dateStarted).toBeTruthy();
     expect(dateEnded).toBeTruthy();
     expect(stage).toBeTruthy();
-    expect(status).toBeTruthy();
   });
 
   it("has no a11y violations", async () => {
