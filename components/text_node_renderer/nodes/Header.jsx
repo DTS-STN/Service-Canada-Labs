b005de4e2659c3f5da74947951f62d6598c8a20c
Jordan <jordan.walcaraz@gmail.com>
2024-09-26 10:07:10 -0400

    add default margin to h3

--- components/text_node_renderer/nodes/Header.jsx
+++ components/text_node_renderer/nodes/Header.jsx
@@ -2,15 +2,21 @@ export default function Header(props) {
   switch (props.node.style) {
     case "h1":
       return (
-        <h1 className={props.index === 0 ? "mt-0" : ""}>{props.children}</h1>
+        <h1 id={props.index} className={props.index === 0 ? "mt-0" : ""}>
+          {props.children}
+        </h1>
       );
     case "h2":
       return (
-        <h2 className={props.index === 0 ? "mt-0" : ""}>{props.children}</h2>
+        <h2 id={props.index} className={props.index === 0 ? "mt-0" : ""}>
+          {props.children}
+        </h2>
       );
     case "h3":
       return (
-        <h3 className={props.index === 0 ? "mt-0" : ""}>{props.children}</h3>
+        <h3 id={props.index} className={props.index === 0 ? "mt-0" : ""}>
+          {props.children}
+        </h3>
       );
     case "h4":
       return <h4>{props.children}</h4>;
