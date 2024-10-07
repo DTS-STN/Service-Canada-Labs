9c4d2aaecc109453b33379077b49955dd3299113
Jordan <jordan.walcaraz@gmail.com>
2024-10-07 10:28:31 -0400

    add comment

--- pages/_app.js
+++ pages/_app.js
@@ -26,6 +26,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
+      {/* Pre-load fonts */}
       <main className={`${notoSans.variable} ${lato.variable}`}>
         <Component {...pageProps} />
       </main>
