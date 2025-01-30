25c9e0c03f6204b57caa5b6727f3a409328fcae5
Jordan <jordan.walcaraz@gmail.com>
2025-01-30 12:08:52 -0500

    test

--- pages/projects/[projectId]/[articleId].js
+++ pages/projects/[projectId]/[articleId].js
@@ -178,7 +178,7 @@ export async function getStaticPaths() {
 
   return {
     paths,
-    fallback: "blocking", // Show loading state for new pages being generated
+    fallback: "blocking",
   };
 }
 
