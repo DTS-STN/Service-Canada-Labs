a91d8fc8adc3a5a34116382b1679c0857b9c8879
Jordan Willis <31868510+will0684@users.noreply.github.com>
2025-02-07 04:00:28 -0800

    Update project updates link to use scTitleEn/scTitleFr (#1182)

--- pages/projects/[projectId]/[articleId].js
+++ pages/projects/[projectId]/[articleId].js
@@ -136,8 +136,8 @@ export default function ArticlePage({ ...props }) {
             // Link to filtered updates page
             href={
               props.locale === "en"
-                ? `/en/updates?project=${pageData.scLabProject.scTermEn}`
-                : `/fr/mises-a-jour?projet=${pageData.scLabProject.scTermFr}`
+                ? `/en/updates?project=${pageData.scLabProject.scTitleEn}`
+                : `/fr/mises-a-jour?projet=${pageData.scLabProject.scTitleFr}`
             }
           />
         ) : null}
