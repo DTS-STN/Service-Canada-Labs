3de394cfe0b698c96c340afe20b7b9135095d7f7
Jordan Willis <31868510+will0684@users.noreply.github.com>
2025-02-18 05:49:01 -0800

    refactor: Improve mobile navigation layout and add Service Canada Labs label (#1193)

--- components/molecules/TopNavBar.js
+++ components/molecules/TopNavBar.js
@@ -50,95 +50,102 @@ export function TopNavBar({
         </div>
       </div>
       {/* Mobile Nav Menu */}
-      <div className="static lg:hidden mt-5 flex flex-col w-full mr-4">
-        <button
-          aria-label={buttonAriaLabel}
-          aria-haspopup="true"
-          aria-expanded={isOpen ? "true" : "false"}
-          aria-controls="menu"
-          onClick={handleClick}
-          className="self-end"
-        >
+      <div className="static lg:hidden mt-4 flex flex-col w-full mr-4">
+        <div className="flex justify-between">
+          <Link href={homeLink} className="ml-4 font-body text-p font-semibold">
+            Service Canada Labs
+          </Link>
+          <button
+            aria-label={buttonAriaLabel}
+            aria-haspopup="true"
+            aria-expanded={isOpen ? "true" : "false"}
+            aria-controls="menu"
+            onClick={handleClick}
+            className="self-end"
+          >
+            {isOpen ? (
+              <div
+                className={`${
+                  isTransitioningClosed ? "fade-out" : "fade-in"
+                } -mt-1 p-1.5`}
+              >
+                <svg
+                  width="20"
+                  height="20"
+                  viewBox="0 0 17 17"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="exit-icon"
+                >
+                  <path d="M11.3775 8.25L16.0683 3.55922C16.6439 2.98359 16.6439 2.05031 16.0683 1.47422L15.0258 0.431719C14.4502 -0.143906 13.5169 -0.143906 12.9408 0.431719L8.25 5.1225L3.55922 0.431719C2.98359 -0.143906 2.05031 -0.143906 1.47422 0.431719L0.431719 1.47422C-0.143906 2.04984 -0.143906 2.98312 0.431719 3.55922L5.1225 8.25L0.431719 12.9408C-0.143906 13.5164 -0.143906 14.4497 0.431719 15.0258L1.47422 16.0683C2.04984 16.6439 2.98359 16.6439 3.55922 16.0683L8.25 11.3775L12.9408 16.0683C13.5164 16.6439 14.4502 16.6439 15.0258 16.0683L16.0683 15.0258C16.6439 14.4502 16.6439 13.5169 16.0683 12.9408L11.3775 8.25Z" />
+                </svg>
+              </div>
+            ) : (
+              <div
+                className={`${isTransitioningClosed ? "fade-in" : ""} -mt-1`}
+              >
+                <svg
+                  width="32"
+                  height="32"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M4 18L20 18"
+                    stroke="#000000"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                  />
+                  <path
+                    d="M4 12L20 12"
+                    stroke="#000000"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                  />
+                  <path
+                    d="M4 6L20 6"
+                    stroke="#000000"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                  />
+                </svg>
+              </div>
+            )}
+          </button>
           {isOpen ? (
-            <div
+            <ul
+              id="menu"
               className={`${
                 isTransitioningClosed ? "fade-out" : "fade-in"
-              } -mt-1 p-1.5`}
+              } absolute w-full flex flex-col mt-8 pb-4 bg-custom-gray-lightest drop-shadow-xl`}
             >
-              <svg
-                width="20"
-                height="20"
-                viewBox="0 0 17 17"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="exit-icon"
+              {/* <li
+                className={`${
+                  isTransitioningClosed ? "decrease-margin" : "expand-margin"
+                } my-2 ml-4 text-[20px]`}
               >
-                <path d="M11.3775 8.25L16.0683 3.55922C16.6439 2.98359 16.6439 2.05031 16.0683 1.47422L15.0258 0.431719C14.4502 -0.143906 13.5169 -0.143906 12.9408 0.431719L8.25 5.1225L3.55922 0.431719C2.98359 -0.143906 2.05031 -0.143906 1.47422 0.431719L0.431719 1.47422C-0.143906 2.04984 -0.143906 2.98312 0.431719 3.55922L5.1225 8.25L0.431719 12.9408C-0.143906 13.5164 -0.143906 14.4497 0.431719 15.0258L1.47422 16.0683C2.04984 16.6439 2.98359 16.6439 3.55922 16.0683L8.25 11.3775L12.9408 16.0683C13.5164 16.6439 14.4502 16.6439 15.0258 16.0683L16.0683 15.0258C16.6439 14.4502 16.6439 13.5169 16.0683 12.9408L11.3775 8.25Z" />
-              </svg>
-            </div>
+                <Link href={homeLink}>{homeLinkLabel}</Link>
+              </li> */}
+              <li
+                className={`${
+                  isTransitioningClosed ? "decrease-margin" : "expand-margin"
+                } my-2 ml-4 text-[20px]`}
+              >
+                <Link href={projectsLink}>{projectsLinkLabel}</Link>
+              </li>
+              <li
+                className={`${
+                  isTransitioningClosed ? "decrease-margin" : "expand-margin"
+                } my-2 ml-4 text-[20px]`}
+              >
+                <Link href={updatesLink}>{updatesLinkLabel}</Link>
+              </li>
+            </ul>
           ) : (
-            <div className={`${isTransitioningClosed ? "fade-in" : ""} -mt-1`}>
-              <svg
-                width="32"
-                height="32"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M4 18L20 18"
-                  stroke="#000000"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                />
-                <path
-                  d="M4 12L20 12"
-                  stroke="#000000"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                />
-                <path
-                  d="M4 6L20 6"
-                  stroke="#000000"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                />
-              </svg>
-            </div>
+            ""
           )}
-        </button>
-        {isOpen ? (
-          <ul
-            id="menu"
-            className={`${
-              isTransitioningClosed ? "fade-out" : "fade-in"
-            } absolute w-full flex flex-col mt-8 pb-4 bg-custom-gray-lightest drop-shadow-xl`}
-          >
-            <li
-              className={`${
-                isTransitioningClosed ? "decrease-margin" : "expand-margin"
-              } my-2 ml-4 text-[20px]`}
-            >
-              <Link href={homeLink}>{homeLinkLabel}</Link>
-            </li>
-            <li
-              className={`${
-                isTransitioningClosed ? "decrease-margin" : "expand-margin"
-              } my-2 ml-4 text-[20px]`}
-            >
-              <Link href={projectsLink}>{projectsLinkLabel}</Link>
-            </li>
-            <li
-              className={`${
-                isTransitioningClosed ? "decrease-margin" : "expand-margin"
-              } my-2 ml-4 text-[20px]`}
-            >
-              <Link href={updatesLink}>{updatesLinkLabel}</Link>
-            </li>
-          </ul>
-        ) : (
-          ""
-        )}
+        </div>
       </div>
     </nav>
   );
